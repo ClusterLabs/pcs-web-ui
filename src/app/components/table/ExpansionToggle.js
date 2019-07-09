58d40abc2f0e47ba37fd75a3822e63f23068542b
Ivan Devat <idevat@redhat.com>
2019-07-09 14:34:17 +0200

    cleanup table's expansion toggle

--- src/app/components/table/ExpansionToggle.js
+++ src/app/components/table/ExpansionToggle.js
@@ -1,18 +1,36 @@
 import React from "react";
 
-export default ({ expanded, onClick, children, ...rest }) => (
-  <td
-    className={
-      `pf-c-table__compound-expansion-toggle ${expanded ? "pf-m-expanded" : ""}`
-    }
-    {...rest}
-  >
-    <button
-      className="pf-c-button pf-m-link"
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  </td>
-);
+export default ({
+  expanded,
+  onClick,
+  children,
+  ...rest
+}) => {
+  const tdClassNames = [
+    "pf-c-table__compound-expansion-toggle",
+  ];
+  if (expanded) {
+    tdClassNames.push("pf-m-expanded");
+  }
+
+  const buttonClassNames = [
+    "pf-c-button",
+    "pf-m-link",
+    "pf-m-gutter",
+    "pf-l-split",
+  ];
+  // TODO display: flex is supposed to be applied by pf-l-split, however
+  // display: inline-block from pf-c-button is applied
+  return (
+    <td className={tdClassNames.join(" ")} {...rest}>
+      <button
+        style={{ display: "flex" }}
+        type="button"
+        className={buttonClassNames.join(" ")}
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    </td>
+  );
+};
