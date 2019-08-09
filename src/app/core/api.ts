570951f9cffe8fa452f630a22d340e6321cc40a3
Ivan Devat <idevat@redhat.com>
2019-08-09 19:35:48 +0200

    cleanup api submodule

--- src/app/core/api.ts
+++
@@ -1,5 +0,0 @@
-import * as error from "./api/errors";
-import * as call from "./api/calls";
-import * as types from "./api/types";
-
-export { error, call, types };
--- src/app/core/apiTypes.ts
+++
@@ -1,4 +0,0 @@
-export interface ApiError {
-  name: string,
-  message: string,
-}
