786e2de7ab950e64c51157205c135064cb5b8185
Ivan Devat <idevat@redhat.com>
2019-08-19 12:43:11 +0200

    fix component index path

---
+++ src/app/components/index.js
@@ -0,0 +1,22 @@
+import BackgroundImage, { BackgroundImageSources } from "./BackgroundImage";
+import PageHeader from "./PageHeader";
+import ClusterPage from "./ClusterPage";
+import Spinner from "./Spinner";
+import PageSectionDataLoading from "./PageSectionDataLoading";
+import StatusSign from "./StatusSign";
+import Table from "./table/Table";
+import InlineAlert from "./InlineAlert";
+import StatusIco from "./StatusIco";
+
+export {
+  BackgroundImage,
+  BackgroundImageSources,
+  ClusterPage,
+  PageHeader,
+  PageSectionDataLoading,
+  Spinner,
+  StatusSign,
+  Table,
+  InlineAlert,
+  StatusIco,
+};
