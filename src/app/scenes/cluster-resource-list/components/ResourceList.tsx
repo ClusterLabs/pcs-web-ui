4ffaa0cd9ade4526d678067db4fc65959ea3fb25
Ivan Devat <idevat@redhat.com>
2019-09-13 19:50:38 +0200

    siplify resource list components

--- src/app/scenes/cluster-resource-list/components/ResourceList.tsx
+++ src/app/scenes/cluster-resource-list/components/ResourceList.tsx
@@ -1,19 +1,22 @@
 import React from "react";
-import { DataList } from "@patternfly/react-core";
+import {
+  DataList,
+  DataListItem,
+  DataListCell,
+} from "@patternfly/react-core";
 
 import { Resource } from "app/services/cluster/types";
-
-import ResourceListItem from "./ResourceListItem";
 
 const ClusterResourceList = ({ resourceList }: {
   resourceList: Resource[],
 }) => (
   <DataList aria-label="Cluster resource list">
     {resourceList.map(resource => (
-      <ResourceListItem
-        key={resource.id}
-        resource={resource}
-      />
+      <DataListItem key={resource.id} aria-labelledby={resource.id}>
+        <DataListCell>
+          {resource.id}
+        </DataListCell>
+      </DataListItem>
     ))}
   </DataList>
 );
