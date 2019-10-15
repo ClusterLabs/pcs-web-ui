2b9d882b76733edbcfa3acdb073e12d381335675
Ivan Devat <idevat@redhat.com>
2019-10-15 15:42:22 +0200

    remove unused components

--- src/app/common/components/UrlTabs.tsx
+++ src/app/common/components/UrlTabs.tsx
@@ -3,25 +3,6 @@ import { useDispatch } from "react-redux";
 import { push } from "connected-react-router";
 import { Tab, Tabs } from "@patternfly/react-core";
 
-// UrlTabs needs map label -> url. Components that uses UrlTabs sometimes need
-// define a map with labels as keys STATICALLY to check types (e.g. keyof typeof
-// labelUrlCreateMap). But the url must be built dynamically (e.g. according
-// current cluster name). One solution is to create map label -> urlCreator and
-// then build label -> url map from it. Intention of this function is help with
-// this process a bit. Look at particular places where it is used...
-function createLabelUrlMap<T extends Record<string, U>, U>(
-  tabUrlCreatorMap: T,
-  toUrl: (urlCreator: U) => string,
-) {
-  return (): Record<keyof T, string> => (Object.keys(tabUrlCreatorMap).reduce(
-    (map, key) => ({
-      ...map,
-      [key]: toUrl(tabUrlCreatorMap[key]),
-    }),
-    {} as Record<keyof T, string>,
-  ));
-}
-
 function UrlTabs<T extends {[key:string]: string}>(
   { tabSettingsMap, currentTab }: {
     tabSettingsMap: T,
@@ -49,7 +30,5 @@ function UrlTabs<T extends {[key:string]: string}>(
     </Tabs>
   );
 }
-
-UrlTabs.createLabelUrlMap = createLabelUrlMap;
 
 export default UrlTabs;
--- src/app/services/cluster/index.ts
+++ src/app/services/cluster/index.ts
@@ -1,12 +1,10 @@
 import reducer from "./reducer";
 import sagas from "./sagas";
-import ClusterView from "./components/ClusterView";
 import IssueList from "./components/IsueList";
 import useClusterState from "./useClusterState";
 import * as selectors from "./selectors";
 
 export {
-  ClusterView,
   IssueList,
   reducer,
   sagas,
