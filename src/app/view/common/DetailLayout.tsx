bf22958d553f06282a44ce2c36ff8ff9d38c60a8
Ivan Devat <idevat@redhat.com>
2019-12-11 14:52:57 +0100

    remove duplicit tag Stack from DetailLayout

--- src/app/view/common/DetailLayout.tsx
+++ src/app/view/common/DetailLayout.tsx
@@ -19,22 +19,20 @@ const DetailLayout = ({
   tabs?: JSX.Element|JSX.Element[]|string|null;
 }>) => (
   <Stack gutter="md" className="pf-u-m-md">
-    <Stack gutter="md" className="pf-u-m-md">
-      <StackItem>
-        <Level>
-          <LevelItem>
-            {caption}
-          </LevelItem>
-          <LevelItem>
-            <Button variant="plain" aria-label="Close panel" onClick={onClose}>
-              <TimesIcon />
-            </Button>
-          </LevelItem>
-        </Level>
-      </StackItem>
-      {tabs && (<StackItem>{tabs}</StackItem>)}
-      {children}
-    </Stack>
+    <StackItem>
+      <Level>
+        <LevelItem>
+          {caption}
+        </LevelItem>
+        <LevelItem>
+          <Button variant="plain" aria-label="Close panel" onClick={onClose}>
+            <TimesIcon />
+          </Button>
+        </LevelItem>
+      </Level>
+    </StackItem>
+    {tabs && (<StackItem>{tabs}</StackItem>)}
+    {children}
   </Stack>
 );
 
