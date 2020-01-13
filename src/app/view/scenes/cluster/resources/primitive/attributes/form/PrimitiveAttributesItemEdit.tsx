7cbba357f251907c637bd4463622abb0b5416dcf
Ivan Devat <idevat@redhat.com>
2020-01-13 09:15:58 +0100

    show form for instance attribute value decision

--- src/app/view/scenes/cluster/resources/primitive/attributes/form/PrimitiveAttributesItemEdit.tsx
+++ src/app/view/scenes/cluster/resources/primitive/attributes/form/PrimitiveAttributesItemEdit.tsx
@@ -43,19 +43,48 @@ const PrimitiveAttributesItemEdit = ({
                 ariaLabel={`Use concurent value: ${remoteValue}`}
                 value={remoteValue}
               >
+                <label
+                  className="pf-c-radio__label"
+                  htmlFor={`remote-value-${label}`}
+                >
+                  Use the new value
+                </label>
+                <br />
                 <strong>{remoteValue}</strong>
               </AttributeDecisionRadio>
             </StackItem>
           </>
         )}
-        <StackItem>
-          <TextInput
-            type="text"
-            id={id}
-            name={id}
-            defaultValue={userValue}
-            onChange={(updatedValue: string) => onChange(label, updatedValue)}
-          />
+        <StackItem className="pf-u-mt-sm">
+          <div className={remoteValue !== initialValue ? "pf-c-radio ha-c-radio" : ""}>
+            {remoteValue !== initialValue && (
+              <input
+                className="pf-c-radio__input"
+                type="radio"
+                id={id}
+                name={label}
+                required
+                aria-label={label}
+              />
+            )}
+            <span className="pf-c-radio__label pf-u-w-100">
+              {remoteValue !== initialValue && (
+                <>
+                  <label htmlFor={id}>Use the following value</label>
+                  <br />
+                </>
+              )}
+              <TextInput
+                type="text"
+                id={id}
+                name={id}
+                defaultValue={userValue}
+                onChange={
+                  (updatedValue: string) => onChange(label, updatedValue)
+                }
+              />
+            </span>
+          </div>
         </StackItem>
       </Stack>
     </AttributeDecisionFormGroup>
