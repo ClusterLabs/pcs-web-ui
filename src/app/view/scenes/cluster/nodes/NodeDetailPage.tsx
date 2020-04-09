d9a451579d34ee312e26614f7befee10aa4b816a
Ivan Devat <idevat@redhat.com>
2020-04-09 16:07:24 +0100

    add detail tab to  nodes detail

--- src/app/view/scenes/cluster/nodes/NodeDetailPage.tsx
+++ src/app/view/scenes/cluster/nodes/NodeDetailPage.tsx
@@ -1,7 +1,24 @@
 import React from "react";
-import { DetailLayout, useGroupDetailViewContext } from "app/view/common";
+import {
+  DetailLayout,
+  UrlTabs,
+  useGroupDetailViewContext,
+} from "app/view/common";
+import { analyzeRoutes, join, useMatch } from "app/view/utils";
 
 export const NodeDetailPage = () => {
-  const { selectedItemUrlName } = useGroupDetailViewContext();
-  return <DetailLayout caption={selectedItemUrlName} />;
+  const { selectedItemUrlName, urlPrefix } = useGroupDetailViewContext();
+
+  const nodeUrlPrefix = join(urlPrefix, selectedItemUrlName);
+
+  const { tab, urlMap } = analyzeRoutes("Detail", {
+    Detail: useMatch({ path: nodeUrlPrefix, exact: true }),
+  });
+
+  return (
+    <DetailLayout
+      caption={selectedItemUrlName}
+      tabs={<UrlTabs tabSettingsMap={urlMap} currentTab={tab} />}
+    />
+  );
 };
