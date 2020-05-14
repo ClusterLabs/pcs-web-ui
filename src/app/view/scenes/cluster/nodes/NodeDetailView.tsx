dd25964af5ce30f7a1281c48019eb54328386180
Ivan Devat <idevat@redhat.com>
2020-05-14 19:23:39 +0100

    improve node detail resources list caption

--- src/app/view/scenes/cluster/nodes/NodeDetailView.tsx
+++ src/app/view/scenes/cluster/nodes/NodeDetailView.tsx
@@ -21,7 +21,7 @@ export const NodeDetailView = ({ node }: { node: types.cluster.Node }) => {
       </StackItem>
       <StackItem>
         <TextContent>
-          <Text component="h1"> Resource status on node </Text>
+          <Text component="h1"> Statuses of resources running on node </Text>
         </TextContent>
         <CrmStatusTable
           crmStatusList={crmStatusList}
