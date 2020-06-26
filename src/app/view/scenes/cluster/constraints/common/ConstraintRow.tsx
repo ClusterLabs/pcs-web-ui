af6ca064f0cb51179f5eaa4abde2c2eaa5b82348
Ivan Devat <idevat@redhat.com>
2020-06-26 19:23:17 +0100

    fix details in ConstraintRow component

--- src/app/view/scenes/cluster/constraints/common/ConstraintRow.tsx
+++ src/app/view/scenes/cluster/constraints/common/ConstraintRow.tsx
@@ -9,33 +9,32 @@ import {
 
 export const ConstraintRow = ({
   id = "",
-  "aria-labelledby": ariaLabelledBy = "Constraint",
   dataListCells,
   content = null,
 }: React.PropsWithChildren<{
   id?: string;
-  "aria-labelledby"?: string;
   content?: JSX.Element | null;
   dataListCells: React.ComponentProps<
     typeof DataListItemCells
   >["dataListCells"];
 }>) => {
   const [showDetails, setShowDetails] = React.useState(false);
+  const rowId = `constraint-${id}-row`;
   return (
-    <DataListItem aria-labelledby={ariaLabelledBy} isExpanded={showDetails}>
-      <DataListItemRow>
+    <DataListItem aria-labelledby={rowId} isExpanded={showDetails}>
+      <DataListItemRow id={rowId}>
         <DataListToggle
           onClick={() => setShowDetails(!showDetails)}
           isExpanded={showDetails}
           id={`constraint-row-${id}`}
-          aria-controls="ex-expand1"
+          aria-controls={`details-constraint-${id}`}
           aria-hidden={content === null}
         />
         <DataListItemCells dataListCells={dataListCells} />
       </DataListItemRow>
       <DataListContent
-        aria-label="Primary Content Details"
-        id="ex-expand1"
+        aria-label={`Details of constraint ${id}`}
+        id={`details-constraint-${id}`}
         isHidden={content === null || !showDetails}
       >
         {content}
