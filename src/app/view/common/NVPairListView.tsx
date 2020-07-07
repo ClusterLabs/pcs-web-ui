524d29271af701b9069809c75b8c5bee8878991f
Ivan Devat <idevat@redhat.com>
2020-07-07 15:01:45 +0200

    add empty state for nvpair list view

--- src/app/view/common/NVPairListView.tsx
+++ src/app/view/common/NVPairListView.tsx
@@ -1,4 +1,11 @@
 import React from "react";
+import {
+  EmptyState,
+  EmptyStateBody,
+  EmptyStateIcon,
+  Title,
+} from "@patternfly/react-core";
+import { PlusCircleIcon } from "@patternfly/react-icons";
 
 import { types } from "app/store";
 
@@ -9,14 +16,25 @@ export const NVPairListView = ({
 }) => {
   return (
     <div className="pf-c-content">
-      <dl>
-        {nvPairListView.map(p => (
-          <React.Fragment key={p.id}>
-            <dt>{p.name}</dt>
-            <dd>{p.value}</dd>
-          </React.Fragment>
-        ))}
-      </dl>
+      {nvPairListView.length === 0 && (
+        <EmptyState style={{ margin: "auto" }}>
+          <EmptyStateIcon icon={PlusCircleIcon} />
+          <Title size="lg" headingLevel="h3">
+            No attribute here.
+          </Title>
+          <EmptyStateBody>No attribute has been added.</EmptyStateBody>
+        </EmptyState>
+      )}
+      {nvPairListView.length > 0 && (
+        <dl>
+          {nvPairListView.map(p => (
+            <React.Fragment key={p.id}>
+              <dt>{p.name}</dt>
+              <dd>{p.value}</dd>
+            </React.Fragment>
+          ))}
+        </dl>
+      )}
     </div>
   );
 };
