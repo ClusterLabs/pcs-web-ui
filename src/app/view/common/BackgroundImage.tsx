0afa0bdc75d55114c1ec461cbe576743f51cae2a
Ivan Devat <idevat@redhat.com>
2020-07-10 14:04:38 +0200

    fix background image

--- src/app/view/common/BackgroundImage.tsx
+++ src/app/view/common/BackgroundImage.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { BackgroundImage as PfBackgroundImage } from "@patternfly/react-core";
+import {
+  BackgroundImageSrcMap,
+  BackgroundImage as PfBackgroundImage,
+} from "@patternfly/react-core";
 
-export const BackgroundImageSources = {
-  xs: "/ui/static/media/pfbg_576.jpg",
-  xs2x: "/ui/static/media/pfbg_576@2x.jpg",
-  sm: "/ui/static/media/pfbg_768.jpg",
-  sm2x: "/ui/static/media/pfbg_768@2x.jpg",
-  lg: "/ui/static/media/pfbg_1200.jpg",
+import b1200 from "@patternfly/patternfly/assets/images/pfbg_1200.jpg";
+import b768 from "@patternfly/patternfly/assets/images/pfbg_768.jpg";
+import b768x2x from "@patternfly/patternfly/assets/images/pfbg_768@2x.jpg";
+import b576 from "@patternfly/patternfly/assets/images/pfbg_576.jpg";
+import b576x2x from "@patternfly/patternfly/assets/images/pfbg_576@2x.jpg";
+
+export const BackgroundImageSources: BackgroundImageSrcMap = {
+  lg: b1200,
+  sm: b768,
+  sm2x: b768x2x,
+  xs: b576,
+  xs2x: b576x2x,
 };
 
 export const BackgroundImage = () => (
