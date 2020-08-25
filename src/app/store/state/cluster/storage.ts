d1dcb70257720edb27dea415fa10c5adb5231db4
Ivan Devat <idevat@redhat.com>
2020-08-25 08:21:49 +0200

    take all actions with cluster name in storage reducer

--- src/app/store/state/cluster/storage.ts
+++ src/app/store/state/cluster/storage.ts
@@ -29,42 +29,32 @@ const clusterStorageItem = combineReducers<ClusterStorageItem>({
   wizardResourceCreate,
 });
 
+function hasProperty<O extends Record<string, unknown>, P extends PropertyKey>(
+  obj: O,
+  prop: P,
+): obj is O & Record<P, unknown> {
+  /* eslint-disable no-prototype-builtins */
+  return obj.hasOwnProperty(prop);
+}
+
 const clusterStorage: Reducer<ClusterStorageMap> = (state = {}, action) => {
-  switch (action.type) {
-    case "CLUSTER_DATA.SYNC":
-    case "CLUSTER_DATA.FETCH.SUCCESS":
-    case "CLUSTER_DATA.FETCH.FAILED":
-    case "RESOURCE_AGENT.LOAD":
-    case "RESOURCE_AGENT.LOAD.SUCCESS":
-    case "RESOURCE_AGENT.LOAD.FAILED":
-    case "FENCE_AGENT.LOAD":
-    case "FENCE_AGENT.LOAD.SUCCESS":
-    case "FENCE_AGENT.LOAD.FAILED":
-    case "RESOURCE_TREE.ITEM.TOGGLE":
-    case "CLUSTER_PROPERTIES.LOAD":
-    case "CLUSTER_PROPERTIES.LOAD.SUCCESS":
-    case "CLUSTER_PROPERTIES.LOAD.FAILED":
-    case "RESOURCE_AGENT_LIST.LOAD":
-    case "RESOURCE_AGENT_LIST.LOAD.SUCCESS":
-    case "RESOURCE_AGENT_LIST.LOAD.FAILED":
-    case "RESOURCE.PRIMITIVE.CREATE":
-    case "RESOURCE.PRIMITIVE.CREATE.SET_AGENT_NAME":
-    case "RESOURCE.PRIMITIVE.CREATE.SET_RESOURCE_NAME":
-    case "RESOURCE.PRIMITIVE.CREATE.SET_INSTANCE_ATTRIBUTE":
-    case "RESOURCE.PRIMITIVE.CREATE.SUCCESS":
-    case "RESOURCE.PRIMITIVE.CREATE.FAILED":
-    case "RESOURCE.PRIMITIVE.CREATE.CLOSE":
-      /* eslint-disable no-case-declarations */
-      const name = action.payload.clusterUrlName;
-      return {
-        ...state,
-        [name]: clusterStorageItem(state[name], action),
-      };
-    case "AUTH.REQUIRED":
-      return {};
-    default:
-      return state;
+  if (action.type === "AUTH.REQUIRED") {
+    return {};
   }
+  if (
+    // TODO - DATA_READING.SET_UP has array in payload; make it standard object!
+    action.type !== "DATA_READING.SET_UP"
+    && hasProperty(action, "payload")
+    && hasProperty(action.payload, "clusterUrlName")
+  ) {
+    const name = action.payload.clusterUrlName;
+    return {
+      ...state,
+      [name]: clusterStorageItem(state[name], action),
+    };
+  }
+
+  return state;
 };
 
 export default clusterStorage;
