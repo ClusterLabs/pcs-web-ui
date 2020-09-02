0bf30c6c10fc0c2d82f81b2658bd9605b9849842
Ivan Devat <idevat@redhat.com>
2020-09-02 08:56:49 +0200

    show basic info about colocation as sentence

--- src/app/scenes/cluster/constraints/colocation/ConstraintRowColocationPair.tsx
+++ src/app/scenes/cluster/constraints/colocation/ConstraintRowColocationPair.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Gallery, GalleryItem } from "@patternfly/react-core";
+import { DataListCell, Gallery, GalleryItem } from "@patternfly/react-core";
 
 import { types, url } from "app/store";
 import { Link, useSelectedClusterName } from "app/view";
@@ -18,16 +18,31 @@ export const ConstraintRowColocationPair = ({
       id={constraint.id}
       dataListCells={
         <>
-          <ConstraintCell label="Type" value="Colocation" />
-          <ConstraintCell label="Resource">
-            <Link to={url.cluster.resources(clusterName, constraint.rsc)} />
-          </ConstraintCell>
-          <ConstraintCell label="With resource">
-            <Link
-              to={url.cluster.resources(clusterName, constraint["with-rsc"])}
-            />
-          </ConstraintCell>
-          <ConstraintCell label="Score" value={constraint.score} />
+          <ConstraintCell label="Type" value="Colocation" width={1} />
+          <DataListCell width={3}>
+            {"Resource "}
+            <strong>
+              <Link to={url.cluster.resources(clusterName, constraint.rsc)} />
+            </strong>
+            {" in role "}
+            <strong>{constraint["rsc-role"] || "Started"}</strong>
+            {constraint.score === "INFINITY"
+            || (constraint.score
+              && typeof constraint.score === "number"
+              && constraint.score > 0)
+              ? " together with "
+              : " apart from "}
+            {" resource "}
+            <strong>
+              <Link
+                to={url.cluster.resources(clusterName, constraint["with-rsc"])}
+              />
+            </strong>
+            {" in role "}
+            <strong>{constraint["with-rsc-role"] || "Started"}</strong>
+          </DataListCell>
+
+          <ConstraintCell label="Score" value={constraint.score} width={1} />
         </>
       }
       content={
