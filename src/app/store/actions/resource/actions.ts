3376f0b69d04444db8e124f2200ed3890815035b
Ivan Devat <idevat@redhat.com>
2020-11-30 13:52:12 +0100

    separate resource actions

---
+++ src/app/store/actions/resource/actions.ts
@@ -0,0 +1,37 @@
+export type ResourceActionsActions = {
+  "RESOURCE.UPDATE_INSTANCE_ATTRIBUTES": {
+    type: "RESOURCE.UPDATE_INSTANCE_ATTRIBUTES";
+    payload: {
+      clusterUrlName: string;
+      resourceId: string;
+      attributes: Record<string, string>;
+    };
+  };
+
+  "RESOURCE.REFRESH": {
+    type: "RESOURCE.REFRESH";
+    payload: {
+      clusterUrlName: string;
+      resourceId: string;
+      resourceType: "resource" | "fence-device";
+    };
+  };
+
+  "RESOURCE.CLEANUP": {
+    type: "RESOURCE.CLEANUP";
+    payload: {
+      clusterUrlName: string;
+      resourceId: string;
+      resourceType: "resource" | "fence-device";
+    };
+  };
+
+  "RESOURCE.DELETE": {
+    type: "RESOURCE.DELETE";
+    payload: {
+      clusterUrlName: string;
+      resourceIds: string[];
+      resourceType: "resource" | "fence-device";
+    };
+  };
+};
--- src/app/store/actions/resource/index.ts
+++ src/app/store/actions/resource/index.ts
@@ -1,41 +1,10 @@
 import { ResourceCreateActions } from "./create";
 import { ResourceTreeActions } from "./tree";
+import { ResourceActionsActions } from "./actions";
 
-export type ResourceActions = ResourceCreateActions &
-  ResourceTreeActions & {
-    "RESOURCE.UPDATE_INSTANCE_ATTRIBUTES": {
-      type: "RESOURCE.UPDATE_INSTANCE_ATTRIBUTES";
-      payload: {
-        clusterUrlName: string;
-        resourceId: string;
-        attributes: Record<string, string>;
-      };
-    };
-
-    "RESOURCE.REFRESH": {
-      type: "RESOURCE.REFRESH";
-      payload: {
-        clusterUrlName: string;
-        resourceId: string;
-        resourceType: "resource" | "fence-device";
-      };
-    };
-
-    "RESOURCE.CLEANUP": {
-      type: "RESOURCE.CLEANUP";
-      payload: {
-        clusterUrlName: string;
-        resourceId: string;
-        resourceType: "resource" | "fence-device";
-      };
-    };
-
-    "RESOURCE.DELETE": {
-      type: "RESOURCE.DELETE";
-      payload: {
-        clusterUrlName: string;
-        resourceIds: string[];
-        resourceType: "resource" | "fence-device";
-      };
-    };
-  };
+// prettier-ignore
+export type ResourceActions = (
+  & ResourceCreateActions
+  & ResourceTreeActions
+  & ResourceActionsActions
+);
