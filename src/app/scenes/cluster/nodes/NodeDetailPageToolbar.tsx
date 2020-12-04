247e923d0afd7d046d2b1b93349f1b689e6da78e
Ivan Devat <idevat@redhat.com>
2020-12-04 10:47:02 +0100

    refactor/unmess detail toolbars

--- src/app/scenes/cluster/nodes/NodeDetailPageToolbar.tsx
+++ src/app/scenes/cluster/nodes/NodeDetailPageToolbar.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { ToolbarGroup, ToolbarItem } from "@patternfly/react-core";
 
 import { Action, types } from "app/store";
 import {
   DetailLayoutToolbar,
   DetailLayoutToolbarAction,
-  DetailLayoutToolbarDropdown,
   useClusterState,
   useSelectedClusterName,
 } from "app/view";
@@ -15,6 +13,7 @@ export const NodeDetailPageToolbar: React.FC<{
 }> = ({ node }) => {
   const clusterUrlName = useSelectedClusterName();
   const { isNodeAttrCibTrue } = useClusterState(clusterUrlName);
+
   const standbyUnstandbyAction = (standby: boolean): Action => ({
     type: "LIB.CALL.CLUSTER",
     payload: {
@@ -26,6 +25,32 @@ export const NodeDetailPageToolbar: React.FC<{
       },
     },
   });
+  const standby: DetailLayoutToolbarAction = {
+    action: standbyUnstandbyAction(true),
+    confirm: {
+      title: "Standby node?",
+      description: (
+        <>
+          Put the node into standby mode. The node will no longer be able to
+          host resources
+        </>
+      ),
+    },
+  };
+
+  const unstandby: DetailLayoutToolbarAction = {
+    action: standbyUnstandbyAction(false),
+    confirm: {
+      title: "Untandby node?",
+      description: (
+        <>
+          Remove the node from standby mode. The node specified will now be able
+          to to host resources
+        </>
+      ),
+    },
+  };
+
   const maintenanceUnmanintenanceAction = (maintenance: boolean): Action => ({
     type: "LIB.CALL.CLUSTER",
     payload: {
@@ -40,113 +65,77 @@ export const NodeDetailPageToolbar: React.FC<{
       },
     },
   });
-  const standbyUnstandbyMenuItem: React.ComponentProps<
-    typeof DetailLayoutToolbarDropdown
-  >["menuItems"] = isNodeAttrCibTrue(node.name, "standby")
-    ? {
-        Unstandby: {
-          confirm: {
-            title: "Untandby node?",
-            description: (
-              <>
-                Remove the node from standby mode. The node specified will now
-                be able to to host resources
-              </>
-            ),
-          },
-          action: standbyUnstandbyAction(false),
-        },
-      }
-    : {
-        Standby: {
-          confirm: {
-            title: "Standby node?",
-            description: (
-              <>
-                Put the node into standby mode. The node will no longer be able
-                to host resources
-              </>
-            ),
-          },
-          action: standbyUnstandbyAction(true),
-        },
-      };
-  const maintenanceUnmanintenanceMenuItem: React.ComponentProps<
-    typeof DetailLayoutToolbarDropdown
-  >["menuItems"] = isNodeAttrCibTrue(node.name, "maintenance")
-    ? {
-        Unmaintenance: {
-          confirm: {
-            title: "Unmaintenance node?",
-            description: "Remove the node into maintenance mode",
-          },
-          action: maintenanceUnmanintenanceAction(false),
-        },
-      }
-    : {
-        Maintenance: {
-          confirm: {
-            title: "Maintenance node?",
-            description: "Put the node into maintenance mode",
-          },
-          action: maintenanceUnmanintenanceAction(true),
-        },
-      };
 
+  const maintenance: DetailLayoutToolbarAction = {
+    action: maintenanceUnmanintenanceAction(true),
+    confirm: {
+      title: "Maintenance node?",
+      description: "Put the node into maintenance mode",
+    },
+  };
+
+  const unmaintenance: DetailLayoutToolbarAction = {
+    action: maintenanceUnmanintenanceAction(false),
+    confirm: {
+      title: "Unmaintenance node?",
+      description: "Remove the node into maintenance mode",
+    },
+  };
+
+  const start: DetailLayoutToolbarAction = {
+    action: {
+      type: "NODE.START",
+      payload: { clusterUrlName, nodeName: node.name },
+    },
+    confirm: {
+      title: "Start node?",
+      description: "Start a cluster on the node",
+    },
+  };
+
+  const stop: DetailLayoutToolbarAction = {
+    action: {
+      type: "NODE.STOP",
+      payload: { clusterUrlName, nodeName: node.name },
+    },
+    confirm: {
+      title: "Stop node?",
+      description: "Stop a cluster on the node",
+    },
+  };
+
+  const remove: DetailLayoutToolbarAction = {
+    action: {
+      type: "LIB.CALL.CLUSTER",
+      payload: {
+        clusterUrlName,
+        taskLabel: `remove node "${node.name}"`,
+        call: {
+          command: "cluster-remove-nodes",
+          payload: { node_list: [node.name] },
+        },
+      },
+    },
+    confirm: {
+      title: "Remove node?",
+      description: "Shutdown specified nodes and remove them from the cluster.",
+    },
+  };
   return (
-    <DetailLayoutToolbar>
-      <ToolbarGroup>
-        <ToolbarItem>
-          <DetailLayoutToolbarAction
-            name="Start"
-            title="Start node?"
-            action={{
-              type: "NODE.START",
-              payload: { clusterUrlName, nodeName: node.name },
-            }}
-          >
-            Start a cluster on the node
-          </DetailLayoutToolbarAction>
-        </ToolbarItem>
-        <ToolbarItem>
-          <DetailLayoutToolbarAction
-            name="Stop"
-            title="Stop node?"
-            action={{
-              type: "NODE.STOP",
-              payload: { clusterUrlName, nodeName: node.name },
-            }}
-          >
-            Stop a cluster on the node
-          </DetailLayoutToolbarAction>
-        </ToolbarItem>
-      </ToolbarGroup>
-      <ToolbarItem>
-        <DetailLayoutToolbarDropdown
-          menuItems={{
-            ...standbyUnstandbyMenuItem,
-            ...maintenanceUnmanintenanceMenuItem,
-            Remove: {
-              action: {
-                type: "LIB.CALL.CLUSTER",
-                payload: {
-                  clusterUrlName,
-                  taskLabel: `remove node "${node.name}"`,
-                  call: {
-                    command: "cluster-remove-nodes",
-                    payload: { node_list: [node.name] },
-                  },
-                },
-              },
-              confirm: {
-                title: "Remove node?",
-                description:
-                  "Shutdown specified nodes and remove them from the cluster.",
-              },
-            },
-          }}
-        />
-      </ToolbarItem>
-    </DetailLayoutToolbar>
+    <DetailLayoutToolbar
+      buttonActions={{
+        start,
+        stop,
+      }}
+      dropdownActions={{
+        ...(isNodeAttrCibTrue(node.name, "standby")
+          ? { unstandby }
+          : { standby }),
+        ...(isNodeAttrCibTrue(node.name, "maintenance")
+          ? { unmaintenance }
+          : { maintenance }),
+        remove,
+      }}
+    />
   );
 };
--- src/app/scenes/cluster/resources/primitive/PrimitivePageToolbar.tsx
+++ src/app/scenes/cluster/resources/primitive/PrimitivePageToolbar.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { ToolbarGroup, ToolbarItem } from "@patternfly/react-core";
 
 import { types } from "app/store";
 import {
   DetailLayoutToolbar,
   DetailLayoutToolbarAction,
-  DetailLayoutToolbarDropdown,
   useSelectedClusterName,
 } from "app/view";
 
@@ -27,192 +25,184 @@ export const PrimitivePageToolbar: React.FC<{
   primitive: Primitive;
 }> = ({ primitive }) => {
   const clusterUrlName = useSelectedClusterName();
-  const isManaged = isPrimitiveManaged(primitive);
-  const isEnabled = isPrimitiveEnabled(primitive);
 
-  const cloneUncloneMenuItem: React.ComponentProps<
-    typeof DetailLayoutToolbarDropdown
-  >["menuItems"] = primitive.inClone
-    ? {
-        Unclone: {
-          confirm: {
-            title: "Unclone resource?",
-            description: (
-              <>
-                Remove the clone which contains the resource (the resource will
-                not be removed).
-              </>
-            ),
-          },
-          action: {
-            type: "RESOURCE.UNCLONE",
-            payload: {
-              clusterUrlName,
-              resourceId: primitive.id,
-            },
-          },
+  const unclone: DetailLayoutToolbarAction = {
+    confirm: {
+      title: "Unclone resource?",
+      description: (
+        <>
+          Remove the clone which contains the resource (the resource will not be
+          removed).
+        </>
+      ),
+    },
+    action: {
+      type: "RESOURCE.UNCLONE",
+      payload: {
+        clusterUrlName,
+        resourceId: primitive.id,
+      },
+    },
+  };
+
+  const clone: DetailLayoutToolbarAction = {
+    confirm: {
+      title: "Clone resource?",
+      description: "Set up the specified resource or group as a clone.",
+    },
+    action: {
+      type: "RESOURCE.CLONE",
+      payload: {
+        clusterUrlName,
+        resourceId: primitive.id,
+      },
+    },
+  };
+
+  const deleteItem: DetailLayoutToolbarAction = {
+    action: {
+      type: "RESOURCE.DELETE",
+      payload: {
+        resourceIds: [primitive.id],
+        clusterUrlName,
+        resourceType: "resource",
+      },
+    },
+    confirm: {
+      title: "Delete resource?",
+      description: <>This deletes the resource</>,
+    },
+  };
+
+  const refresh: DetailLayoutToolbarAction = {
+    action: {
+      type: "RESOURCE.REFRESH",
+      payload: {
+        resourceId: primitive.id,
+        clusterUrlName,
+        resourceType: "resource",
+      },
+    },
+    confirm: {
+      title: "Refresh resource?",
+      description: (
+        <>
+          This makes the cluster forget the complete operation history
+          (including failures) of the resource and re-detects its current state.
+        </>
+      ),
+    },
+  };
+
+  const cleanup: DetailLayoutToolbarAction = {
+    action: {
+      type: "RESOURCE.CLEANUP",
+      payload: {
+        resourceId: primitive.id,
+        clusterUrlName,
+        resourceType: "resource",
+      },
+    },
+    confirm: {
+      title: "Cleanup resource?",
+      description: (
+        <>
+          This makes the cluster forget failed operations from history of the
+          resource and re-detects its current state.
+        </>
+      ),
+    },
+  };
+
+  const unmanage: DetailLayoutToolbarAction = {
+    action: {
+      type: "LIB.CALL.CLUSTER",
+      payload: {
+        clusterUrlName,
+        taskLabel: `unmanage "${primitive.id}"`,
+        call: {
+          command: "resource-unmanage",
+          payload: { resource_or_tag_ids: [primitive.id] },
         },
-      }
-    : {
-        Clone: {
-          confirm: {
-            title: "Clone resource?",
-            description: "Set up the specified resource or group as a clone.",
-          },
-          action: {
-            type: "RESOURCE.CLONE",
-            payload: {
-              clusterUrlName,
-              resourceId: primitive.id,
-            },
-          },
+      },
+    },
+    confirm: {
+      title: "Unmanage resource?",
+      description: "This disallows the cluster to start and stop the resource",
+    },
+  };
+
+  const manage: DetailLayoutToolbarAction = {
+    action: {
+      type: "LIB.CALL.CLUSTER",
+      payload: {
+        clusterUrlName,
+        taskLabel: `manage "${primitive.id}"`,
+        call: {
+          command: "resource-manage",
+          payload: { resource_or_tag_ids: [primitive.id] },
         },
-      };
+      },
+    },
+    confirm: {
+      title: "Manage resource?",
+      description: "This allows the cluster to start and stop the resource",
+    },
+  };
+
+  const disable: DetailLayoutToolbarAction = {
+    action: {
+      type: "LIB.CALL.CLUSTER",
+      payload: {
+        clusterUrlName,
+        taskLabel: `disable "${primitive.id}"`,
+        call: {
+          command: "resource-disable",
+          payload: { resource_or_tag_ids: [primitive.id] },
+        },
+      },
+    },
+    confirm: {
+      title: "Disable resource?",
+      description: (
+        <>
+          This attempts to stop the resource if they are running and forbid the
+          cluster from starting it again.
+        </>
+      ),
+    },
+  };
+
+  const enable: DetailLayoutToolbarAction = {
+    action: {
+      type: "LIB.CALL.CLUSTER",
+      payload: {
+        clusterUrlName,
+        taskLabel: `enable "${primitive.id}"`,
+        call: {
+          command: "resource-enable",
+          payload: { resource_or_tag_ids: [primitive.id] },
+        },
+      },
+    },
+    confirm: {
+      title: "Enable resource?",
+      description: "This allows the cluster to start the resource",
+    },
+  };
+
   return (
-    <DetailLayoutToolbar>
-      <ToolbarGroup>
-        <ToolbarItem>
-          {isManaged && (
-            <DetailLayoutToolbarAction
-              name="Unmanage"
-              title="Unmanage resource?"
-              action={{
-                type: "LIB.CALL.CLUSTER",
-                payload: {
-                  clusterUrlName,
-                  taskLabel: `unmanage "${primitive.id}"`,
-                  call: {
-                    command: "resource-unmanage",
-                    payload: { resource_or_tag_ids: [primitive.id] },
-                  },
-                },
-              }}
-            >
-              This disallows the cluster to start and stop the resource
-            </DetailLayoutToolbarAction>
-          )}
-          {!isManaged && (
-            <DetailLayoutToolbarAction
-              name="Manage"
-              title="Manage resource?"
-              action={{
-                type: "LIB.CALL.CLUSTER",
-                payload: {
-                  clusterUrlName,
-                  taskLabel: `manage "${primitive.id}"`,
-                  call: {
-                    command: "resource-manage",
-                    payload: { resource_or_tag_ids: [primitive.id] },
-                  },
-                },
-              }}
-            >
-              This allows the cluster to start and stop the resource
-            </DetailLayoutToolbarAction>
-          )}
-        </ToolbarItem>
-        <ToolbarItem>
-          {isEnabled && (
-            <DetailLayoutToolbarAction
-              name="Disable"
-              title="Disable resource?"
-              action={{
-                type: "LIB.CALL.CLUSTER",
-                payload: {
-                  clusterUrlName,
-                  taskLabel: `disable "${primitive.id}"`,
-                  call: {
-                    command: "resource-disable",
-                    payload: { resource_or_tag_ids: [primitive.id] },
-                  },
-                },
-              }}
-            >
-              This attempts to stop the resource if they are running and forbid
-              the cluster from starting it again.
-            </DetailLayoutToolbarAction>
-          )}
-          {!isEnabled && (
-            <DetailLayoutToolbarAction
-              name="Enable"
-              title="Enable resource?"
-              action={{
-                type: "LIB.CALL.CLUSTER",
-                payload: {
-                  clusterUrlName,
-                  taskLabel: `enable "${primitive.id}"`,
-                  call: {
-                    command: "resource-enable",
-                    payload: { resource_or_tag_ids: [primitive.id] },
-                  },
-                },
-              }}
-            >
-              This allows the cluster to start the resource
-            </DetailLayoutToolbarAction>
-          )}
-        </ToolbarItem>
-      </ToolbarGroup>
-      <ToolbarItem>
-        <DetailLayoutToolbarDropdown
-          menuItems={{
-            Refresh: {
-              action: {
-                type: "RESOURCE.REFRESH",
-                payload: {
-                  resourceId: primitive.id,
-                  clusterUrlName,
-                  resourceType: "resource",
-                },
-              },
-              confirm: {
-                title: "Refresh resource?",
-                description: (
-                  <>
-                    This makes the cluster forget the complete operation history
-                    (including failures) of the resource and re-detects its
-                    current state.
-                  </>
-                ),
-              },
-            },
-            Cleanup: {
-              action: {
-                type: "RESOURCE.CLEANUP",
-                payload: {
-                  resourceId: primitive.id,
-                  clusterUrlName,
-                  resourceType: "resource",
-                },
-              },
-              confirm: {
-                title: "Cleanup resource?",
-                description: (
-                  <>
-                    This makes the cluster forget failed operations from history
-                    of the resource and re-detects its current state.
-                  </>
-                ),
-              },
-            },
-            ...cloneUncloneMenuItem,
-            Delete: {
-              action: {
-                type: "RESOURCE.DELETE",
-                payload: {
-                  resourceIds: [primitive.id],
-                  clusterUrlName,
-                  resourceType: "resource",
-                },
-              },
-              confirm: {
-                title: "Delete resource?",
-                description: <>This deletes the resource</>,
-              },
-            },
-          }}
-        />
-      </ToolbarItem>
-    </DetailLayoutToolbar>
+    <DetailLayoutToolbar
+      buttonActions={{
+        ...(isPrimitiveManaged(primitive) ? { unmanage } : { manage }),
+        ...(isPrimitiveEnabled(primitive) ? { disable } : { enable }),
+      }}
+      dropdownActions={{
+        refresh,
+        cleanup,
+        ...(primitive.inClone ? { unclone } : { clone }),
+        delete: deleteItem,
+      }}
+    />
   );
 };
