60500da28c5dcac29cfb9e11deaa11cca726ab93
Ivan Devat <idevat@redhat.com>
2021-03-17 20:08:46 +0100

    improve action export from app/store

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,37 +1,22 @@
 import React from "react";
 import { useDispatch as useReduxDispatch, useSelector } from "react-redux";
 
-import {
-  Action as TAction,
-  ActionLeaf as TActionLeaf,
-  ActionMap as TActionMap,
-  actionNewId,
-} from "./actions";
+import { Action } from "./actions";
 import * as selectors from "./selectors";
 import * as types from "./state/types";
 import * as utils from "./utils";
 import * as location from "./location";
 import { setupStore } from "./store";
 
+export * from "./actions";
+
 export const useDispatch = () => {
   const reduxDispatch = useReduxDispatch();
   const dispatch = React.useCallback(
-    (action: TAction) => reduxDispatch<TAction>(action),
+    (action: Action) => reduxDispatch<Action>(action),
     [reduxDispatch],
   );
   return dispatch;
 };
 
-export type Action = TAction;
-export type ActionMap = TActionMap;
-export type ActionLeaf = TActionLeaf;
-
-export {
-  actionNewId,
-  setupStore,
-  useSelector,
-  selectors,
-  types,
-  utils,
-  location,
-};
+export { setupStore, useSelector, selectors, types, utils, location };
