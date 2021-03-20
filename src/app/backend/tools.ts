8b82ee8831c754a650e32974e1bcaa7a0780af2e
Ivan Devat <idevat@redhat.com>
2021-03-20 09:54:54 +0100

    remove unnecessary exports from backend/tools

--- src/app/backend/tools.ts
+++ src/app/backend/tools.ts
@@ -3,8 +3,6 @@ import * as t from "io-ts";
 import * as api from "./call";
 import * as validate from "./validate";
 import * as http from "./http";
-import * as lib from "./lib";
 import { endpoints } from "./endpoints";
 
-const { get, post } = http;
-export { api, endpoints, http, get, post, validate, t, lib };
+export { api, endpoints, http, validate, t };
