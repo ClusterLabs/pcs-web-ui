1c33d9d47c05553df196512590528573edc1842a
Ivan Devat <idevat@redhat.com>
2021-03-30 18:16:07 +0200

    consolide constraint types on backend

--- src/app/backend/endpoints/clusterStatus/shape/constraints.ts
+++ src/app/backend/endpoints/clusterStatus/shape/constraints.ts
@@ -23,14 +23,6 @@ const ApiConstraintAction = t.keyof({
   stop: null,
 });
 
-const ApiConstraintLocationRscDiscovery = t.keyof({
-  always: null,
-  never: null,
-  exclusive: null,
-});
-
-const ApiConstraintIdReference = t.type({ "id-ref": ApiId });
-
 const ApiConstraintTicketLossPolicy = t.keyof({
   stop: null,
   demote: null,
@@ -43,26 +35,24 @@ const ApiConstraintResourceSetOrdering = t.keyof({
   listed: null,
 });
 
-const ApiConstraintResourceSetStructured = t.intersection([
-  t.type({
-    id: ApiId,
-    resources: t.array(ApiResourceId),
-  }),
-  t.partial({
-    // sequential and require-all should be booleans, however, strings come
-    // from backend
-    sequential: t.string,
-    "require-all": t.string,
-    ordering: ApiConstraintResourceSetOrdering,
-    action: ApiConstraintAction,
-    role: ApiConstraintRole,
-    score: ApiScore,
-  }),
-]);
-
 const ApiConstraintResourceSet = t.union([
-  ApiConstraintIdReference,
-  ApiConstraintResourceSetStructured,
+  t.type({ "id-ref": ApiId }),
+  t.intersection([
+    t.type({
+      id: ApiId,
+      resources: t.array(ApiResourceId),
+    }),
+    t.partial({
+      // sequential and require-all should be booleans, however, strings come
+      // from backend
+      sequential: t.string,
+      "require-all": t.string,
+      ordering: ApiConstraintResourceSetOrdering,
+      action: ApiConstraintAction,
+      role: ApiConstraintRole,
+      score: ApiScore,
+    }),
+  ]),
 ]);
 
 /*
@@ -71,155 +61,136 @@ TODO add attributes according .rng. (to ApiLocation, ApiResourceAttributes, ...)
 warning: there are relations between attributes - things will be more
 complicated!
 */
-
-/*
-rule_string does not follow rng schema, however the ruby backend does it that
-way.
-In backend sets inside locations are ignored. Location constraint with resource
-set are not sent.
-Id is:
-  - id of constraint
-    - in ApiConstraintLocationNode
-    - in ApiConstraintLocationRule with id-ref
-  - id of rule
-    - in ApiConstraintLocationRule with score or score attribute (i.e. without
-      id-ref)
-*/
-const ApiConstraintLocation = t.intersection([
-  t.union([t.type({ rsc: t.string }), t.type({ "rsc-pattern": t.string })]),
-  t.partial({
-    role: ApiConstraintRole,
-    "resource-discovery": ApiConstraintLocationRscDiscovery,
-  }),
-]);
-
-const ApiConstraintLocationNode = t.intersection([
-  t.type({ id: ApiId }),
-  ApiConstraintLocation,
-  t.type({
-    node: t.string,
-    score: ApiScore,
-  }),
-]);
-
-/*
-It is not the full common rule. It is just shortened version which attributes
-are mixed into location constraint in the backend.
-*/
-const ApiConstraintLocationRule = t.intersection([
-  ApiConstraintLocation,
-  t.type({ rule_string: t.string }),
-  t.union([
-    ApiConstraintIdReference,
-    t.intersection([
-      t.type({ id: ApiId }),
-      t.union([
-        t.type({ score: ApiScore }),
-        t.type({ "score-attribute": t.string }),
-      ]),
-      t.partial({ role: t.string }),
-    ]),
-  ]),
-]);
-
-/*
-The element lifetime with rules is not currently present in the backend
-response.
-*/
-const ApiConstraintColocation = t.intersection([
-  t.type({ id: ApiId }),
-  t.partial({ score: ApiScore }),
-]);
-
-const ApiConstraintColocationPair = t.intersection([
-  ApiConstraintColocation,
-  t.type({
-    rsc: t.string,
-    "with-rsc": t.string,
-  }),
-  t.partial({
-    "node-attribute": t.string,
-    "rsc-role": ApiConstraintRole,
-    "with-rsc-role": ApiConstraintRole,
-    "rsc-instance": t.number,
-    "with-rsc-instance": t.number,
-  }),
-]);
-
-const ApiConstraintColocationSet = t.intersection([
-  ApiConstraintColocation,
-  t.type({ sets: t.array(ApiConstraintResourceSet) }),
-]);
-
-const ApiConstraintOrder = t.intersection([
-  t.type({ id: ApiId }),
-  t.partial({
-    // symmetrical and require-all should be booleans, however, strings come
-    // from backend
-    symmetrical: t.string,
-    "require-all": t.string,
-  }),
-  t.union([
-    t.partial({ score: ApiScore }),
-    t.partial({
-      kind: t.keyof({
-        Optional: null,
-        Mandatory: null,
-        Serialize: null,
-      }),
-    }),
-  ]),
-]);
-
-const ApiConstraintOrderSet = t.intersection([
-  ApiConstraintOrder,
-  t.type({
-    sets: t.array(ApiConstraintResourceSet),
-  }),
-]);
-
-const ApiConstraintOrderPair = t.intersection([
-  ApiConstraintOrder,
-  t.type({
-    first: ApiId,
-    then: ApiId,
-  }),
-  t.partial({
-    "first-action": ApiConstraintAction,
-    "then-action": ApiConstraintAction,
-    "first-instance": t.number,
-    "then-instance": t.number,
-  }),
-]);
-
-const ApiConstraintTicket = t.intersection([
-  t.type({
-    id: ApiId,
-    ticket: t.string,
-  }),
-  t.partial({ "loss-policy": ApiConstraintTicketLossPolicy }),
-]);
-
-const ApiConstraintTicketResource = t.intersection([
-  ApiConstraintTicket,
-  t.type({ rsc: t.string }),
-  t.partial({ "rsc-role": ApiConstraintRole }),
-]);
-
-const ApiConstraintTicketSet = t.intersection([
-  ApiConstraintTicket,
-  t.type({ sets: t.array(ApiConstraintResourceSet) }),
-]);
-
 export const ApiConstraints = t.partial({
   rsc_location: t.array(
-    t.union([ApiConstraintLocationNode, ApiConstraintLocationRule]),
+    /*
+      rule_string does not follow rng schema, however the ruby backend does it
+      that way.  In backend sets inside locations are ignored. Location
+      constraint with resource set are not sent.  Id is:
+        - id of constraint
+          - in ApiConstraintLocationNode
+          - in ApiConstraintLocationRule with id-ref
+        - id of rule
+          - in ApiConstraintLocationRule with score or score attribute (i.e.
+            without id-ref)
+      */
+    t.intersection([
+      t.union([t.type({ rsc: t.string }), t.type({ "rsc-pattern": t.string })]),
+      t.partial({
+        role: ApiConstraintRole,
+        "resource-discovery": t.keyof({
+          always: null,
+          never: null,
+          exclusive: null,
+        }),
+      }),
+      t.union([
+        t.intersection([
+          t.type({ id: ApiId }),
+          t.type({
+            node: t.string,
+            score: ApiScore,
+          }),
+        ]),
+        t.intersection([
+          /*
+            It is not the full common rule. It is just shortened version which
+            attributes are mixed into location constraint in the backend.
+          */
+          t.type({ rule_string: t.string }),
+          t.union([
+            t.type({ "id-ref": ApiId }),
+            t.intersection([
+              t.type({ id: ApiId }),
+              t.union([
+                t.type({ score: ApiScore }),
+                t.type({ "score-attribute": t.string }),
+              ]),
+              t.partial({ role: t.string }),
+            ]),
+          ]),
+        ]),
+      ]),
+    ]),
   ),
   rsc_colocation: t.array(
-    t.union([ApiConstraintColocationPair, ApiConstraintColocationSet]),
+    /*
+    The element lifetime with rules is not currently present in the backend
+    response.
+    */
+    t.intersection([
+      t.type({ id: ApiId }),
+      t.partial({ score: ApiScore }),
+      t.union([
+        t.type({ sets: t.array(ApiConstraintResourceSet) }),
+        t.intersection([
+          t.type({
+            rsc: t.string,
+            "with-rsc": t.string,
+          }),
+          t.partial({
+            "node-attribute": t.string,
+            "rsc-role": ApiConstraintRole,
+            "with-rsc-role": ApiConstraintRole,
+            "rsc-instance": t.number,
+            "with-rsc-instance": t.number,
+          }),
+        ]),
+      ]),
+    ]),
   ),
-  rsc_order: t.array(t.union([ApiConstraintOrderPair, ApiConstraintOrderSet])),
+  rsc_order: t.array(
+    t.intersection([
+      t.type({ id: ApiId }),
+      t.partial({
+        // symmetrical and require-all should be booleans, however, strings come
+        // from backend
+        symmetrical: t.string,
+        "require-all": t.string,
+      }),
+      t.union([
+        t.partial({ score: ApiScore }),
+        t.partial({
+          kind: t.keyof({
+            Optional: null,
+            Mandatory: null,
+            Serialize: null,
+          }),
+        }),
+      ]),
+      t.union([
+        t.type({
+          sets: t.array(ApiConstraintResourceSet),
+        }),
+        t.intersection([
+          t.type({
+            first: ApiId,
+            then: ApiId,
+          }),
+          t.partial({
+            "first-action": ApiConstraintAction,
+            "then-action": ApiConstraintAction,
+            "first-instance": t.number,
+            "then-instance": t.number,
+          }),
+        ]),
+      ]),
+    ]),
+  ),
   rsc_ticket: t.array(
-    t.union([ApiConstraintTicketResource, ApiConstraintTicketSet]),
+    t.intersection([
+      t.type({
+        id: ApiId,
+        ticket: t.string,
+      }),
+      t.partial({ "loss-policy": ApiConstraintTicketLossPolicy }),
+      t.union([
+        t.type({ sets: t.array(ApiConstraintResourceSet) }),
+        t.intersection([
+          t.type({ rsc: t.string }),
+          t.partial({ "rsc-role": ApiConstraintRole }),
+        ]),
+      ]),
+    ]),
   ),
 });
