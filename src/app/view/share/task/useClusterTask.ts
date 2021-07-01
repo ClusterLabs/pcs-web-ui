8c68f2ed0ecf08ef8232697186ec87af8fbe844c
Ivan Devat <idevat@redhat.com>
2021-07-01 20:45:00 +0200

    remove wizard context from useClusterTask

--- src/app/view/share/task/useClusterTask.ts
+++ src/app/view/share/task/useClusterTask.ts
@@ -1,6 +1,3 @@
-import React from "react";
-import { WizardContext } from "@patternfly/react-core";
-
 import { selectors } from "app/store";
 import { useDispatch } from "app/view/share/useDispatch";
 import { useClusterSelector } from "app/view/share/useClusterSelector";
@@ -13,11 +10,8 @@ export function useClusterTask<
   const [state, clusterName] = useClusterSelector(selectors.getTask(name));
   const dispatch = useDispatch();
   const openClose = useTaskOpenClose(name);
-  const pfWizardContext = React.useContext(WizardContext);
 
   return {
-    // don't spread wizardContext to avoid conflict if patternfly adds something
-    wizard: pfWizardContext,
     name,
     state,
     ...openClose,
