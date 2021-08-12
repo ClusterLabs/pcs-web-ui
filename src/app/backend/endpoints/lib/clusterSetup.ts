081e25274a85eee7a92c1ccb683f845259d6ea72
Ivan Devat <idevat@redhat.com>
2021-08-12 18:03:12 +0200

    use string types for cluster setup "number" values
    Numbers are not enough (so some kind of validation is necessary) and it
    just complicates the code.

--- src/app/backend/endpoints/lib/clusterSetup.ts
+++ src/app/backend/endpoints/lib/clusterSetup.ts
@@ -20,8 +20,8 @@ export const clusterSetup = endpoint({
       }[];
       compression_options?: {
         model?: string; //a compression model e.g. zlib, lz4 or bzip2
-        threshold?: number; // nonnegative integer
-        level?: number; // nonnegative integer
+        threshold?: string; // nonnegative integer
+        level?: string; // nonnegative integer
       };
       crypto_options?: {
         model?: "nss" | "openssl";
@@ -30,28 +30,28 @@ export const clusterSetup = endpoint({
       };
       totem_options?: {
         block_unlisted_ips?: "yes" | "no";
-        consensus?: number; // nonnegative integer
-        downcheck?: number; // nonnegative integer
-        fail_recv_const?: number; // nonnegative integer
-        heartbeat_failures_allowed?: number; // nonnegative integer
-        hold?: number; // nonnegative integer
-        join?: number; // nonnegative integer
-        max_messages?: number; // nonnegative integer
-        max_network_delay?: number; // nonnegative integer
-        merge?: number; // nonnegative integer
-        miss_count_const?: number; // nonnegative integer
-        send_join?: number; // nonnegative integer
-        seqno_unchanged_const?: number; // nonnegative integer
-        token?: number; // nonnegative integer
-        token_coefficient?: number; // nonnegative integer
-        token_retransmit?: number; // nonnegative integer
-        token_retransmits_before_loss_const?: number; // nonnegative integer
-        window_size?: number; // nonnegative integer
+        consensus?: string; // nonnegative integer
+        downcheck?: string; // nonnegative integer
+        fail_recv_const?: string; // nonnegative integer
+        heartbeat_failures_allowed?: string; // nonnegative integer
+        hold?: string; // nonnegative integer
+        join?: string; // nonnegative integer
+        max_messages?: string; // nonnegative integer
+        max_network_delay?: string; // nonnegative integer
+        merge?: string; // nonnegative integer
+        miss_count_const?: string; // nonnegative integer
+        send_join?: string; // nonnegative integer
+        seqno_unchanged_const?: string; // nonnegative integer
+        token?: string; // nonnegative integer
+        token_coefficient?: string; // nonnegative integer
+        token_retransmit?: string; // nonnegative integer
+        token_retransmits_before_loss_const?: string; // nonnegative integer
+        window_size?: string; // nonnegative integer
       };
       quorum_options?: {
         auto_tie_breaker?: "0" | "1";
         last_man_standing?: "0" | "1";
-        last_man_standing_window?: number; // positive integer
+        last_man_standing_window?: string; // positive integer
         wait_for_all?: "0" | "1";
       };
       wait?: boolean;
@@ -64,17 +64,17 @@ export const clusterSetup = endpoint({
           transport_type: "knet";
           transport_options?: {
             ip_version?: "ipv4" | "ipv6" | "ipv4-6" | "ipv6-4";
-            knet_pmtud_interval?: number; // positive integer
+            knet_pmtud_interval?: string; // positive integer
             link_mode?: "" | "active" | "passive" | "rr";
           };
           link_list: {
-            linknumber: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
-            link_priority?: number; // 0-255
-            mcastport?: number; //a port number (1..65535)
-            ping_interval?: number; // nonnegative integer
-            ping_precision?: number; // nonnegative integer
-            ping_timeout?: number; // nonnegative integer
-            pong_count?: number; // nonnegative integer
+            linknumber: "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7";
+            link_priority?: string; // 0-255
+            mcastport?: string; //a port number (1..65535)
+            ping_interval?: string; // nonnegative integer
+            ping_precision?: string; // nonnegative integer
+            ping_timeout?: string; // nonnegative integer
+            pong_count?: string; // nonnegative integer
             transport?: "sctp" | "udp";
           }[];
         }
@@ -82,14 +82,14 @@ export const clusterSetup = endpoint({
           transport_type: "udp" | "udpu";
           transport_options?: {
             ip_version?: "ipv4" | "ipv6" | "ipv4-6" | "ipv6-4";
-            netmtu?: number; // positive integer
+            netmtu?: string; // positive integer
           };
           link_list: {
             bindnetaddr: string; // ip address
             broadcast: "0" | "1";
             mcastaddr: string; // ip address
-            mcastport: number; //a port number (1..65535)
-            ttl: number; // 0-255,
+            mcastport: string; //a port number (1..65535)
+            ttl: string; // 0-255,
           }[];
         }
     );
