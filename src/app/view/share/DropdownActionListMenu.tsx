11299ca40277b6bdf18b2b609518c6f3f8fa8aa3
Ivan Devat <idevat@redhat.com>
2022-01-10 17:18:50 +0100

    fix file formating

--- src/app/view/share/DropdownActionListMenu.tsx
+++ src/app/view/share/DropdownActionListMenu.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { ActionListItem, Button, Dropdown, DropdownItem, KebabToggle, Modal } from "@patternfly/react-core";
+import {
+  ActionListItem,
+  Button,
+  Dropdown,
+  DropdownItem,
+  KebabToggle,
+  Modal,
+} from "@patternfly/react-core";
 
 import { useDispatch } from "app/view/share/useDispatch";
 
@@ -17,11 +24,11 @@ const capitalize = (s: string) => s[0].toUpperCase() + s.slice(1);
 export const DropdownActionListMenu: React.FC<{
   dropdownActions: ModalActionMap;
 }> = ({ dropdownActions }) => {
-    const [kebabOpen, setKebabOpen] = React.useState(false);
-    const [confirm, setConfirm] = React.useState<ConfirmData | null>(null);
-    const dispatch = useDispatch();
+  const [kebabOpen, setKebabOpen] = React.useState(false);
+  const [confirm, setConfirm] = React.useState<ConfirmData | null>(null);
+  const dispatch = useDispatch();
 
-    const prepareOnClick = (action: ModalAction, name: string) =>
+  const prepareOnClick = (action: ModalAction, name: string) =>
     "confirm" in action
       ? () =>
           setConfirm({
@@ -31,61 +38,61 @@ export const DropdownActionListMenu: React.FC<{
           })
       : action.onClick;
 
-    const closeConfirm = () => setConfirm(null);
+  const closeConfirm = () => setConfirm(null);
 
-    return (
-      <>
+  return (
+    <>
       <ActionListItem>
-          <Dropdown
-            toggle={<KebabToggle onToggle={() => setKebabOpen(!kebabOpen)} />}
-            onSelect={() => setKebabOpen(false)}
-            isOpen={kebabOpen}
-            isPlain
-            dropdownItems={Object.keys(dropdownActions).map(name => (
-              <DropdownItem
-                key={name}
-                component="button"
-                onClick={prepareOnClick(dropdownActions[name], name)}
-                data-test={`${name}-cluster`}
-              >
-                {capitalize(name)}
-              </DropdownItem>
-            ))}
-            position="right"
-          />
+        <Dropdown
+          toggle={<KebabToggle onToggle={() => setKebabOpen(!kebabOpen)} />}
+          onSelect={() => setKebabOpen(false)}
+          isOpen={kebabOpen}
+          isPlain
+          dropdownItems={Object.keys(dropdownActions).map(name => (
+            <DropdownItem
+              key={name}
+              component="button"
+              onClick={prepareOnClick(dropdownActions[name], name)}
+              data-test={`${name}-cluster`}
+            >
+              {capitalize(name)}
+            </DropdownItem>
+          ))}
+          position="right"
+        />
       </ActionListItem>
 
       {confirm && (
-      <Modal
-      variant="small"
-      title={confirm.confirm.title}
-      isOpen
-      onClose={closeConfirm}
-      actions={[
-        <Button
-          key="confirm"
-          variant="primary"
-          onClick={() => {
-            dispatch(confirm.action);
-            closeConfirm();
-          }}
-          data-test="confirm"
+        <Modal
+          variant="small"
+          title={confirm.confirm.title}
+          isOpen
+          onClose={closeConfirm}
+          actions={[
+            <Button
+              key="confirm"
+              variant="primary"
+              onClick={() => {
+                dispatch(confirm.action);
+                closeConfirm();
+              }}
+              data-test="confirm"
+            >
+              {capitalize(confirm.name)}
+            </Button>,
+            <Button
+              key="cancel"
+              variant="link"
+              onClick={closeConfirm}
+              data-test="cancel"
+            >
+              Cancel
+            </Button>,
+          ]}
         >
-          {capitalize(confirm.name)}
-        </Button>,
-        <Button
-          key="cancel"
-          variant="link"
-          onClick={closeConfirm}
-          data-test="cancel"
-        >
-          Cancel
-        </Button>,
-      ]}
-      >
-      {confirm.confirm.description}
-      </Modal>
+          {confirm.confirm.description}
+        </Modal>
       )}
-      </>
-    );
+    </>
+  );
 };
