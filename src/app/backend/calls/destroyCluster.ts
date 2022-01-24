2b653bb2aa62936bfffe2273304b6c4c15750683
Ivan Devat <idevat@redhat.com>
2022-01-24 14:08:54 +0100

    fix backend parameter "all" in cluster destroy

--- src/app/backend/calls/destroyCluster.ts
+++ src/app/backend/calls/destroyCluster.ts
@@ -4,5 +4,5 @@ const { url } = endpoints.destroyCluster;
 
 export const destroyCluster = (clusterName: string): CallResult =>
   http.post(url({ clusterName }), {
-    params: [["--all", "1"]],
+    params: [["all", "1"]],
   });
