52420698424e6f7a5465e1f94b1d208976c78815
Ivan Devat <idevat@redhat.com>
2022-03-04 18:12:21 +0100

    display error on clone of fence device detail

--- src/app/view/cluster/resources/clone/ClonePage.tsx
+++ src/app/view/cluster/resources/clone/ClonePage.tsx
@@ -1,3 +1,5 @@
+import { Alert } from "@patternfly/react-core";
+
 import { Clone } from "app/view/cluster/types";
 import {
   DetailLayout,
@@ -8,24 +10,47 @@ import {
   useUrlTabs,
 } from "app/view/share";
 
-import { CloneDetailWrapper } from "./CloneDetailWrapper";
+import { CloneDetail } from "./CloneDetail";
 
 const tabList = ["detail", "meta"] as const;
 
 export const ClonePage = ({ clone }: { clone: Clone }) => {
   const { currentTab, matchedContext } = useUrlTabs(tabList);
+  if (clone.member.itemType !== "fence-device") {
+    return (
+      <DetailLayout
+        caption={<ResourceDetailCaption resourceId={clone.id} type="clone" />}
+        tabs={<UrlTabs tabList={tabList} currentTab={currentTab} />}
+        data-test={`resource-detail ${clone.id}`}
+      >
+        <Router base={matchedContext}>
+          {currentTab === "detail" && (
+            <CloneDetail
+              id={clone.id}
+              member={clone.member}
+              issueList={clone.issueList}
+            />
+          )}
+          {currentTab === "meta" && (
+            <NVPairListView nvPairListView={clone.metaAttributes} />
+          )}
+        </Router>
+      </DetailLayout>
+    );
+  }
   return (
     <DetailLayout
       caption={<ResourceDetailCaption resourceId={clone.id} type="clone" />}
-      tabs={<UrlTabs tabList={tabList} currentTab={currentTab} />}
       data-test={`resource-detail ${clone.id}`}
     >
-      <Router base={matchedContext}>
-        {currentTab === "detail" && <CloneDetailWrapper clone={clone} />}
-        {currentTab === "meta" && (
-          <NVPairListView nvPairListView={clone.metaAttributes} />
-        )}
-      </Router>
+      <Alert
+        variant="danger"
+        isInline
+        title="Unsupported clone of fence device"
+      >
+        Cloned fence device is not supported. Please unclone this fence device
+        via pcs.
+      </Alert>
     </DetailLayout>
   );
 };
