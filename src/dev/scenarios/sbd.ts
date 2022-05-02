7c8170b9a4aa7d7beedaacdf096db07d026214ff
Ivan Devat <idevat@redhat.com>
2022-05-02 15:14:23 +0200

    add dev scenario for sbd disable

--- src/dev/scenarios/sbd.ts
+++ src/dev/scenarios/sbd.ts
@@ -3,6 +3,22 @@ import * as response from "dev/responses";
 import * as t from "dev/responses/clusterStatus/tools";
 import * as shortcut from "dev/shortcuts";
 
+type LibStdErrors = Exclude<
+  Parameters<typeof shortcut.libStd>[0]["errors"],
+  undefined
+>;
+type LibReport = LibStdErrors[keyof LibStdErrors][number];
+
+const offlineNodesReport: LibReport = {
+  severity: { level: "ERROR", force_code: "SKIP_OFFLINE_NODES" },
+  message: {
+    code: "DEFAULT_FORCIBLE_ERROR",
+    message: "Default forcible error from devel server",
+    payload: { error: "default" },
+  },
+  context: null,
+};
+
 app.libCluster("sbd-enable-sbd", (req, res) => {
   if (req.params.clusterName === "error") {
     res.status(400).send("Error enabling sbd");
@@ -13,19 +29,7 @@ app.libCluster("sbd-enable-sbd", (req, res) => {
       ? "offline-nodes"
       : "ok",
     res,
-    errors: {
-      "offline-nodes": [
-        {
-          severity: { level: "ERROR", force_code: "SKIP_OFFLINE_NODES" },
-          message: {
-            code: "DEFAULT_FORCIBLE_ERROR",
-            message: "Default forcible error from devel server",
-            payload: { error: "default" },
-          },
-          context: null,
-        },
-      ],
-    },
+    errors: { "offline-nodes": [offlineNodesReport] },
   });
 });
 
@@ -35,13 +39,21 @@ app.libCluster("sbd-disable-sbd", (req, res) => {
     return;
   }
   shortcut.libStd({
-    code: req.body.ignore_offline_nodes,
+    code: req.params.clusterName === "error-offline" ? "offline-nodes" : "ok",
     res,
+    errors: { "offline-nodes": [offlineNodesReport] },
   });
 });
 
 shortcut.dashboard([
   t.cluster("error", "error", {
+    node_list: [
+      t.node("1", { sbd_config: null }),
+      t.node("2", { status: "offline", quorum: false }),
+      t.node("3", { status: "unknown" }),
+    ],
+  }),
+  t.cluster("error-offline", "error", {
     node_list: [
       t.node("1", { sbd_config: null }),
       t.node("2", { status: "offline", quorum: false }),
