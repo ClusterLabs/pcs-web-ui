99906400442574c9f0fd22c6948c6cc96194811a
Ivan Devat <idevat@redhat.com>
2023-02-08 11:17:02 +0100

    add cockpit location layer

--- cockpit/@types/Cockpit/index.d.ts
+++ cockpit/@types/Cockpit/index.d.ts
@@ -23,7 +23,7 @@ type HttpOperations = {
 // indicate that the file is a module
 // https://stackoverflow.com/a/59499895
 // https://stackoverflow.com/a/42257742
-export { };
+export {};
 
 declare global {
   type Cockpit = {
@@ -31,6 +31,16 @@ declare global {
       _endpoint: string | number,
       _options?: HttpOptions,
     ) => HttpOperations;
+    location: {
+      href: string;
+      go: (_path: string, _opts?: Record<string, string | string[]>) => void;
+      replace: (
+        _path: string,
+        _opts?: Record<string, string | string[]>,
+      ) => void;
+    };
+    addEventListener: (_type: string, _handler: () => void) => void;
+    removeEventListener: (_type: string, _handler: () => void) => void;
   };
 
   /* eslint-disable-next-line  */
