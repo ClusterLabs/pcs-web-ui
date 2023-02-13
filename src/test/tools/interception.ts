80d484595abaaa2f0b1a206b2c708b4c067bcd4d
Ivan Devat <idevat@redhat.com>
2023-02-13 17:41:16 +0100

    fix tests

--- src/test/tools/interception.ts
+++ src/test/tools/interception.ts
@@ -47,6 +47,7 @@ type RequestCheck = {
 const isAppLoadingUrl = (url: string) =>
   /\/images\/favicon\.png/.exec(url)
   || (/\/ui\//.exec(url) && !/\/ui\/(login|logout)/.exec(url))
+  || /\/base1\/cockpit\.js/.exec(url)
   || /\/static\//.exec(url);
 
 const isRegExp = (candidate: unknown): candidate is RegExp =>
