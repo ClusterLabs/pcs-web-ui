85e42974e493412647a07f41d87e19460ba4d891
Ivan Devat <idevat@redhat.com>
2023-02-23 15:01:20 +0100

    remove .env files support

--- config/paths.js
+++ config/paths.js
@@ -41,7 +41,6 @@ const resolveModule = (resolveFn, filePath) => {
 
 // config after eject: we're in ./config/
 module.exports = {
-  dotenv: resolveApp(".env"),
   appPath: resolveApp("."),
   appBuild: resolveApp(buildPath),
   appPublic: resolveApp("public"),
