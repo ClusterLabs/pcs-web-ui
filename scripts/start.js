45d215c89792afb9f5b58d7e1ad905c7ae647ac3
Ivan Devat <idevat@redhat.com>
2023-02-23 10:18:39 +0100

    remove CI support

--- scripts/start.js
+++ scripts/start.js
@@ -104,14 +104,6 @@ checkBrowsers(paths.appPath, isInteractive)
         process.exit();
       });
     });
-
-    if (process.env.CI !== "true") {
-      // Gracefully exit when stdin ends
-      process.stdin.on("end", function () {
-        devServer.close();
-        process.exit();
-      });
-    }
   })
   .catch(err => {
     if (err && err.message) {
