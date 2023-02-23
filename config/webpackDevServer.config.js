c713cceae3d668c8308e837a9bc25f632d4e0f52
Ivan Devat <idevat@redhat.com>
2023-02-23 08:58:37 +0100

    cleanup webpackDevServer config

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -1,21 +1,23 @@
-'use strict';
+/* eslint-disable @typescript-eslint/no-var-requires */
 
-const fs = require('fs');
-const evalSourceMapMiddleware = require('react-dev-utils/evalSourceMapMiddleware');
-const noopServiceWorkerMiddleware = require('react-dev-utils/noopServiceWorkerMiddleware');
-const ignoredFiles = require('react-dev-utils/ignoredFiles');
-const redirectServedPath = require('react-dev-utils/redirectServedPathMiddleware');
-const paths = require('./paths');
-const getHttpsConfig = require('./getHttpsConfig');
+const fs = require("fs");
 
-const host = process.env.HOST || '0.0.0.0';
+const evalSourceMapMiddleware = require("react-dev-utils/evalSourceMapMiddleware");
+const noopServiceWorkerMiddleware = require("react-dev-utils/noopServiceWorkerMiddleware");
+const ignoredFiles = require("react-dev-utils/ignoredFiles");
+const redirectServedPath = require("react-dev-utils/redirectServedPathMiddleware");
+
+const paths = require("./paths");
+const getHttpsConfig = require("./getHttpsConfig");
+
+const host = process.env.HOST || "0.0.0.0";
 const sockHost = process.env.WDS_SOCKET_HOST;
 const sockPath = process.env.WDS_SOCKET_PATH; // default: '/ws'
 const sockPort = process.env.WDS_SOCKET_PORT;
 
 module.exports = function (proxy, allowedHost) {
   const disableFirewall =
-    !proxy || process.env.DANGEROUSLY_DISABLE_HOST_CHECK === 'true';
+    !proxy || process.env.DANGEROUSLY_DISABLE_HOST_CHECK === "true";
   return {
     // WebpackDevServer 2.4.3 introduced a security fix that prevents remote
     // websites from potentially accessing local content through DNS rebinding:
@@ -35,29 +37,30 @@ module.exports = function (proxy, allowedHost) {
     // really know what you're doing with a special environment variable.
     // Note: ["localhost", ".localhost"] will support subdomains - but we might
     // want to allow setting the allowedHosts manually for more complex setups
-    allowedHosts: disableFirewall ? 'all' : [allowedHost],
+    allowedHosts: disableFirewall ? "all" : [allowedHost],
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': '*',
-      'Access-Control-Allow-Headers': '*',
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
+      "Access-Control-Allow-Headers": "*",
     },
     // Enable gzip compression of generated files.
     compress: true,
     static: {
-      // By default WebpackDevServer serves physical files from current directory
-      // in addition to all the virtual build products that it serves from memory.
-      // This is confusing because those files won’t automatically be available in
-      // production build folder unless we copy them. However, copying the whole
-      // project directory is dangerous because we may expose sensitive files.
-      // Instead, we establish a convention that only files in `public` directory
-      // get served. Our build script will copy `public` into the `build` folder.
-      // In `index.html`, you can get URL of `public` folder with %PUBLIC_URL%:
-      // <link rel="icon" href="%PUBLIC_URL%/favicon.ico">
+      // By default WebpackDevServer serves physical files from current
+      // directory in addition to all the virtual build products that it serves
+      // from memory. This is confusing because those files won’t automatically
+      // be available in production build folder unless we copy them. However,
+      // copying the whole project directory is dangerous because we may expose
+      // sensitive files. Instead, we establish a convention that only files in
+      // `public` directory get served. Our build script will copy `public` into
+      // the `build` folder. In `index.html`, you can get URL of `public` folder
+      // with %PUBLIC_URL%: <link rel="icon" href="%PUBLIC_URL%/favicon.ico">
       // In JavaScript code, you can access it with `process.env.PUBLIC_URL`.
       // Note that we only recommend to use `public` folder as an escape hatch
       // for files like `favicon.ico`, `manifest.json`, and libraries that are
-      // for some reason broken when imported through webpack. If you just want to
-      // use an image, put it in `src` and `import` it from JavaScript instead.
+      // for some reason broken when imported through webpack. If you just want
+      // to use an image, put it in `src` and `import` it from JavaScript
+      // instead.
       directory: paths.appPublic,
       publicPath: [paths.publicUrlOrPath],
       // By default files from `contentBase` will not trigger a page reload.
@@ -71,9 +74,9 @@ module.exports = function (proxy, allowedHost) {
     },
     client: {
       webSocketURL: {
-        // Enable custom sockjs pathname for websocket connection to hot reloading server.
-        // Enable custom sockjs hostname, pathname and port for websocket connection
-        // to hot reloading server.
+        // Enable custom sockjs pathname for websocket connection to hot
+        // reloading server. Enable custom sockjs hostname, pathname and port
+        // for websocket connection to hot reloading server.
         hostname: sockHost,
         pathname: sockPath,
         port: sockPort,
@@ -84,10 +87,10 @@ module.exports = function (proxy, allowedHost) {
       },
     },
     devMiddleware: {
-      // It is important to tell WebpackDevServer to use the same "publicPath" path as
-      // we specified in the webpack config. When homepage is '.', default to serving
-      // from the root.
-      // remove last slash so user can land on `/test` instead of `/test/`
+      // It is important to tell WebpackDevServer to use the same "publicPath"
+      // path as we specified in the webpack config. When homepage is '.',
+      // default to serving from the root. remove last slash so user can land
+      // on `/test` instead of `/test/`
       publicPath: paths.publicUrlOrPath.slice(0, -1),
     },
 
@@ -102,9 +105,9 @@ module.exports = function (proxy, allowedHost) {
     // `proxy` is run between `before` and `after` `webpack-dev-server` hooks
     proxy,
     onBeforeSetupMiddleware(devServer) {
-      // Keep `evalSourceMapMiddleware`
-      // middlewares before `redirectServedPath` otherwise will not have any effect
-      // This lets us fetch source contents from webpack for the error overlay
+      // Keep `evalSourceMapMiddleware` middlewares before `redirectServedPath`
+      // otherwise will not have any effect. This lets us fetch source contents
+      // from webpack for the error overlay
       devServer.app.use(evalSourceMapMiddleware(devServer));
 
       if (fs.existsSync(paths.proxySetup)) {
@@ -113,7 +116,8 @@ module.exports = function (proxy, allowedHost) {
       }
     },
     onAfterSetupMiddleware(devServer) {
-      // Redirect to `PUBLIC_URL` or `homepage` from `package.json` if url not match
+      // Redirect to `PUBLIC_URL` or `homepage` from `package.json` if url not
+      // match
       devServer.app.use(redirectServedPath(paths.publicUrlOrPath));
 
       // This service worker file is effectively a 'no-op' that will reset any
