290b749bdd0dd16b4840ff652eb95c42f17a7236
Ivan Devat <idevat@redhat.com>
2023-02-27 18:56:14 +0100

    remove service worker from webpackDevServer

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -2,7 +2,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 const evalSourceMapMiddleware = require("react-dev-utils/evalSourceMapMiddleware");
-const noopServiceWorkerMiddleware = require("react-dev-utils/noopServiceWorkerMiddleware");
 const ignoredFiles = require("react-dev-utils/ignoredFiles");
 const redirectServedPath = require("react-dev-utils/redirectServedPathMiddleware");
 
@@ -110,12 +109,6 @@ module.exports = function (proxy, allowedHost, publicPath = "/") {
         evalSourceMapMiddleware(devServer),
         // Redirect to `publicPath` if url not match
         redirectServedPath(publicPath),
-        // This service worker file is effectively a 'no-op' that will reset any
-        // previous service worker registered for the same host:port
-        // combination. We do this in development to avoid hitting the
-        // production cache if it used the same host and port.
-        // https://github.com/facebook/create-react-app/issues/2272#issuecomment-302832432
-        noopServiceWorkerMiddleware(publicPath),
       );
 
       return middlewares;
