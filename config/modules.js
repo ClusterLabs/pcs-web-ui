2bb11dd06c8f9bc8eaae24642f49b18ead1516c0
Ivan Devat <idevat@redhat.com>
2023-02-27 14:59:42 +0100

    don't check typescript in config/modules

--- config/modules.js
+++ config/modules.js
@@ -95,42 +95,18 @@ function getJestAliases(options = {}) {
 }
 
 function getModules() {
-  // Check if TypeScript is setup
-  const hasTsConfig = fs.existsSync(paths.appTsConfig);
-  const hasJsConfig = fs.existsSync(paths.appJsConfig);
+  const ts = require(resolve.sync("typescript", {
+    basedir: paths.appNodeModules,
+  }));
 
-  if (hasTsConfig && hasJsConfig) {
-    throw new Error(
-      "You have both a tsconfig.json and a jsconfig.json. If you are using TypeScript please remove your jsconfig.json file.",
-    );
-  }
-
-  let config;
-
-  // If there's a tsconfig.json we assume it's a
-  // TypeScript project and set up the config
-  // based on tsconfig.json
-  if (hasTsConfig) {
-    const ts = require(resolve.sync("typescript", {
-      basedir: paths.appNodeModules,
-    }));
-    config = ts.readConfigFile(paths.appTsConfig, ts.sys.readFile).config;
-    // Otherwise we'll check if there is jsconfig.json
-    // for non TS projects.
-  } else if (hasJsConfig) {
-    config = require(paths.appJsConfig);
-  }
-
-  config = config || {};
-  const options = config.compilerOptions || {};
-
-  const additionalModulePaths = getAdditionalModulePaths(options);
+  const options =
+    ts.readConfigFile(paths.appTsConfig, ts.sys.readFile).config
+      .compilerOptions || {};
 
   return {
-    additionalModulePaths: additionalModulePaths,
+    additionalModulePaths: getAdditionalModulePaths(options),
     webpackAliases: getWebpackAliases(options),
     jestAliases: getJestAliases(options),
-    hasTsConfig,
   };
 }
 
