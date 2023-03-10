bfb73e7880a843ffce77df6f7b39de7563a6135a
Ivan Devat <idevat@redhat.com>
2023-03-10 13:35:43 +0100

    reduce deps: figure out local ip in shell

--- .bin/dev-server.sh
+++ .bin/dev-server.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-export PORT="${PORT:-3000}"
+# Dev server will be started on this HOST and PORT
 export HOST="${HOST:-0.0.0.0}"
+export PORT="${PORT:-3000}"
+# Dev server proxifies backend requests to PCSD_DEV_BACKEND.
 export PCSD_DEV_BACKEND="${PCSD_DEV_BACKEND:-http://localhost:5000}"
+# Dev server uses ALLOWED_HOST for preventing remotes to access local content.
+# The logic (originally js using npm module "address") is extracted from CRA.
+export ALLOWED_HOST="${ALLOWED_HOST:-$(ip addr show |
+	grep 'inet ' |
+	grep -vE 'inet 127' |
+	head -n1 |
+	awk '{print $2}' |
+	sed 's|/.*||')}"
 
 if lsof -i :"$PORT" >/dev/null; then
 	printf "Port %s is occupied.\n\n" "$PORT"
