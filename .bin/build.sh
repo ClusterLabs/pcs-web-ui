2e5ba759f1cfc51e8a13e7648ee1426223415b2d
Ivan Devat <idevat@redhat.com>
2023-03-31 17:48:42 +0200

    stop images removing

--- .bin/build.sh
+++ .bin/build.sh
@@ -106,23 +106,3 @@ restore_node_modules \
 # shellcheck disable=SC1090
 echo ""
 . "$(dirname "$0")/get-build-sizes.sh"
-
-# Some files are removed from build directory:
-# service-worker.js
-# precache-manifest.*.js (information about URLs that need to be precached)
-#   It doesn't work with self signed certificates and `pcsd` daemon uses it by
-#   default.
-#   SecurityError: Failed to register a ServiceWorker:
-#   An SSL certificate error occurred when fetching the script.
-#   https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin
-# asset-manifest.json
-#   It should be useful for integrations. However, there is not a known use case
-#   for it now.
-#   https://github.com/facebook/create-react-app/issues/600
-# images/favicon.png
-#   Current (default) pcs web ui provide its own /images/favicon. No need to
-#   duplicate it.
-#
-rm -f build/precache-manifest.*.js
-rm -f build/images/favicon.png
-find build/images -type d -empty -delete
