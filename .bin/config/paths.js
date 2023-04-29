ef0c3bc077d23ef009dda2c42668781944435966
Ivan Devat <idevat@redhat.com>
2023-04-29 09:45:11 +0200

    don't use cwd for paths

--- .bin/config/paths.js
+++ .bin/config/paths.js
@@ -2,13 +2,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 const path = require("path");
-const fs = require("fs");
 
 module.exports = Object.entries(require("./paths.json")).reduce(
   (allPaths, [key, relativePath]) => {
     return {
       ...allPaths,
-      [key]: path.resolve(fs.realpathSync(process.cwd()), relativePath),
+      [key]: path.resolve(`${__dirname}/../../${relativePath}`),
     };
   },
   {},
