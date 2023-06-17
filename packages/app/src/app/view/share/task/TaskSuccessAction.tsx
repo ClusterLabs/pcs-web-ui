b4e3396873171ebe612c3ff28b6f0f2056fc9e5e
Ivan Devat <idevat@redhat.com>
2023-06-17 17:59:05 +0200

    fix task secondary success action

--- packages/app/src/app/view/share/task/TaskSuccessAction.tsx
+++ packages/app/src/app/view/share/task/TaskSuccessAction.tsx
@@ -1,3 +1,5 @@
+import {Button} from "@patternfly/react-core";
+
 import {ButtonWithEnter} from "app/view/share/ButtonWithEnter";
 
 import {useTaskContext} from "./TaskContext";
@@ -9,13 +11,17 @@ export const TaskSuccessAction = (props: {
   "data-test"?: string;
 }) => {
   const {close} = useTaskContext();
+
+  const ButtonComponent =
+    props.variant === "primary" ? ButtonWithEnter : Button;
+
   return (
-    <ButtonWithEnter
+    <ButtonComponent
       variant={props.variant ?? "primary"}
       onClick={props.action ?? close}
       data-test={props["data-test"] ?? "task-close"}
     >
       {props.label ?? "Close"}
-    </ButtonWithEnter>
+    </ButtonComponent>
   );
 };
