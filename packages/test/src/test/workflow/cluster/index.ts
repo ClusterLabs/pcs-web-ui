d316bb144c183d9eb358701738790df51ceda525
Ivan Devat <idevat@redhat.com>
2023-08-05 18:50:27 +0200

    remove unused test tools

--- packages/test/src/test/workflow/cluster/index.ts
+++ packages/test/src/test/workflow/cluster/index.ts
@@ -1,28 +1,14 @@
 import {clusterAppTabList} from "app/view/cluster/";
 
-import {location, selectors} from "test/tools";
+import {selectors} from "test/tools";
 
 export * as fenceDevices from "./fenceDevices";
 export * as resources from "./resources";
 export * as nodes from "./nodes";
 export * as nvsets from "./nvsets";
-export * as acl from "./acl";
 
 type ClusterTabName = (typeof clusterAppTabList)[number];
 
 export const selectTab = async (tabName: ClusterTabName) => {
   await page.click(selectors.dt("tabs cluster", tabName));
-};
-
-export const goTo = async ({
-  clusterName,
-  tabName,
-}: {
-  clusterName: string;
-  tabName?: ClusterTabName;
-}) => {
-  await page.goto(location.cluster({clusterName}));
-  if (tabName) {
-    await selectTab("acl");
-  }
 };
--- packages/test/src/test/workflow/task/index.ts
+++ packages/test/src/test/workflow/task/index.ts
@@ -1,9 +1,7 @@
 export * from "./common";
 export {task as clusterSetup} from "./clusterSetup";
 export {task as clusterStop} from "./clusterStop";
-export {task as permissionEdit} from "./permissionEdit";
 export {task as importExistingCluster} from "./importExistingCluster";
 export {task as fenceDeviceCreate} from "./fenceDeviceCreate";
 export {task as resourceCreate} from "./resourceCreate";
 export {task as nvsetListModify} from "./nvsetListModify";
-export * from "./acl";
