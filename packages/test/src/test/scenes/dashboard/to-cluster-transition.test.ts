8ceda195d78c6651dd7a70814347065cf4e16c5e
Ivan Devat <idevat@redhat.com>
2023-08-25 16:41:52 +0200

    simplify test

--- packages/test/src/test/scenes/dashboard/to-cluster-transition.test.ts
+++ packages/test/src/test/scenes/dashboard/to-cluster-transition.test.ts
@@ -3,7 +3,7 @@ import * as cs from "dev/responses/clusterStatus/tools";
 import {assert, mock} from "test/tools";
 
 const {clusterBreadcrumbs} = marks;
-const {overview, nodes, resources, fenceDevices} = marks.cluster;
+const {nodes, resources, fenceDevices} = marks.cluster;
 const {cluster: importedCluster} = marks.dashboard.clusterList;
 
 const clusterName = "ok";
@@ -12,16 +12,16 @@ const nodeName = `node-${nodeNumber}`;
 const resourceId = "R1";
 const fenceDeviceId = "F1";
 
-const inTheCluster = (search: (c: typeof importedCluster) => Mark) =>
-  item.byName(importedCluster, clusterName, search);
-
 const clusterListLoaded = async () => {
   await goToDashboard();
   await isVisible(item.byName(importedCluster, clusterName, c => c.nodeCount));
 };
 
-const expectOnTheCluster = async () => {
+const expectOnTheCluster = async (
+  search: (c: typeof marks.cluster) => Mark,
+) => {
   await assert.textIs(clusterBreadcrumbs.clusterName, clusterName);
+  await isVisible(search(marks.cluster));
 };
 
 const clusterStatus = cs.cluster(clusterName, "ok", {
@@ -37,8 +37,7 @@ describe("To cluster transition", () => {
     await clusterListLoaded();
 
     await click(item.byName(importedCluster, clusterName, c => c.name));
-    await expectOnTheCluster();
-    await isVisible(overview);
+    await expectOnTheCluster(c => c.overview);
 
     await click(clusterBreadcrumbs.dashboard);
     await isVisible(marks.dashboard);
@@ -48,10 +47,13 @@ describe("To cluster transition", () => {
     mock.shortcuts.withCluster({clusterStatus});
     await clusterListLoaded();
 
-    await click(inTheCluster(c => c.nodeCount));
-    await click(inTheCluster(c => item.byName(c.node, nodeName, n => n.name)));
-    await expectOnTheCluster();
-    await isVisible(nodes);
+    await click(
+      item.byName(importedCluster, clusterName, [
+        c => c.nodeCount,
+        c => item.byName(c.node, nodeName, n => n.name),
+      ]),
+    );
+    await expectOnTheCluster(c => c.nodes);
     await assert.textIs(nodes.currentNode.name, nodeName);
   });
 
@@ -62,12 +64,13 @@ describe("To cluster transition", () => {
     });
 
     await clusterListLoaded();
-    await click(inTheCluster(c => c.resourceCount));
     await click(
-      inTheCluster(c => item.byId(c.resource, resourceId, r => r.id)),
+      item.byName(importedCluster, clusterName, [
+        c => c.resourceCount,
+        c => item.byId(c.resource, resourceId, r => r.id),
+      ]),
     );
-    await expectOnTheCluster();
-    await isVisible(resources);
+    await expectOnTheCluster(c => c.resources);
     await assert.textIs(resources.currentPrimitive.id, resourceId);
   });
 
@@ -78,12 +81,13 @@ describe("To cluster transition", () => {
     });
 
     await clusterListLoaded();
-    await click(inTheCluster(c => c.fenceDeviceCount));
     await click(
-      inTheCluster(c => item.byId(c.fenceDevice, fenceDeviceId, f => f.id)),
+      item.byName(importedCluster, clusterName, [
+        c => c.fenceDeviceCount,
+        c => item.byId(c.fenceDevice, fenceDeviceId, f => f.id),
+      ]),
     );
-    await expectOnTheCluster();
-    await isVisible(fenceDevices);
+    await expectOnTheCluster(c => c.fenceDevices);
     await assert.textIs(fenceDevices.currentFenceDevice.id, fenceDeviceId);
   });
 });
