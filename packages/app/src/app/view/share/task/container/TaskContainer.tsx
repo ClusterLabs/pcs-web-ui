708f076e7eff86e4b461f33cc025e9b850c358f1
Ivan Devat <idevat@redhat.com>
2023-10-30 15:14:19 +0100

    remove forgotten console.log

--- packages/app/src/app/view/share/task/container/TaskContainer.tsx
+++ packages/app/src/app/view/share/task/container/TaskContainer.tsx
@@ -6,7 +6,6 @@ import {taskMap} from "./taskMap";
 
 export const TaskContainer = () => {
   const currentTask = useSelector(selectors.getCurrentTaskKey);
-  console.log(currentTask);
 
   if (currentTask !== null && currentTask in taskMap) {
     const Task = taskMap[currentTask as keyof typeof taskMap];
