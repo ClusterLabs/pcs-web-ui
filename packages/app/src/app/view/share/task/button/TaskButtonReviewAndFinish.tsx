85584ed147c206159208a44acc4ba1790a4e90b0
Ivan Devat <idevat@redhat.com>
2023-11-14 11:39:21 +0100

    feat: allow skip advanced settings in move/ban

--- packages/app/src/app/view/share/task/button/TaskButtonReviewAndFinish.tsx
+++ packages/app/src/app/view/share/task/button/TaskButtonReviewAndFinish.tsx
@@ -1,18 +1,41 @@
 import {Button, WizardContextConsumer} from "@patternfly/react-core";
 
+import {useDispatch} from "app/view/share/useDispatch";
+
+import {useTaskContext} from "../TaskContext";
+
 export const TaskButtonReviewAndFinish = (props: {
   disabled?: boolean;
   onClick?: () => void;
+  runIf?: boolean;
   label?: string;
   "data-test"?: string;
 }) => {
+  const {task} = useTaskContext();
+  const dispatch = useDispatch();
   return (
     <WizardContextConsumer>
       {({goToStepByName}) => (
         <Button
           variant="tertiary"
           type="submit"
-          onClick={props.onClick ?? (() => goToStepByName("Review"))}
+          onClick={
+            props.onClick
+            ?? (() => {
+              if (props.runIf || props.runIf === undefined) {
+                dispatch({
+                  type: "TASK.VALIDATION.HIDE",
+                  key: {task},
+                });
+                goToStepByName("Review");
+              } else {
+                dispatch({
+                  type: "TASK.VALIDATION.SHOW",
+                  key: {task},
+                });
+              }
+            })
+          }
           data-test={props["data-test"] ?? "review-and-finish"}
           isDisabled={props.disabled ?? false}
         >
--- packages/app/src/app/view/task/resourceBan/NodeFooter.tsx
+++ packages/app/src/app/view/task/resourceBan/NodeFooter.tsx
@@ -2,13 +2,15 @@ import {testMarks} from "app/view/dataTest";
 import {
   TaskButtonBack,
   TaskButtonCancel,
+  TaskButtonReviewAndFinish,
   TaskButtonWizardNext,
   TaskFooter,
 } from "app/view/share";
 
 import {useTask} from "./useTask";
 
-const {next, back, cancel} = testMarks.task.resourceBan.nodeFooter;
+const {next, back, cancel, reviewAndFinish} =
+  testMarks.task.resourceBan.nodeFooter;
 
 export const NodeFooter = () => {
   const {isNodeSettingConsistent} = useTask();
@@ -16,6 +18,11 @@ export const NodeFooter = () => {
     <TaskFooter {...testMarks.task.resourceBan.nodeFooter.mark}>
       <TaskButtonWizardNext actionIf={isNodeSettingConsistent} {...next.mark} />
       <TaskButtonBack disabled={true} {...back.mark} />
+      <TaskButtonReviewAndFinish
+        runIf={isNodeSettingConsistent}
+        label="Review and ban resource"
+        {...reviewAndFinish.mark}
+      />
       <TaskButtonCancel {...cancel.mark} />
     </TaskFooter>
   );
