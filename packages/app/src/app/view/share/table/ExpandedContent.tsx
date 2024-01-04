ed0cf1e7f8b040b4272e28107645292beb80ee95
Ivan Devat <idevat@redhat.com>
2024-01-04 17:25:18 +0100

    fix: use padding for expanded content in dashboard

--- packages/app/src/app/view/share/table/ExpandedContent.tsx
+++ packages/app/src/app/view/share/table/ExpandedContent.tsx
@@ -4,13 +4,11 @@ import {ExpandableRowContent, Td, Tr} from "@patternfly/react-table";
 export const ExpandedContent = ({
   colSpan,
   children,
-  padding = false,
 }: React.PropsWithChildren<{
   colSpan: number;
-  padding?: boolean;
 }>) => (
   <Tr isExpanded>
-    <Td colSpan={colSpan} noPadding={!padding}>
+    <Td colSpan={colSpan}>
       <ExpandableRowContent>{children}</ExpandableRowContent>
     </Td>
   </Tr>
