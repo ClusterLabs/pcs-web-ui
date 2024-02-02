b3203ed73cc91c27236927e5c38d87f891157fb8
Ivan Devat <idevat@redhat.com>
2024-02-02 17:19:55 +0100

    fix: remove nexus proxy from package-lock files

--- .bin/pre-commit.sh
+++ .bin/pre-commit.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-for package in "app" "dev" "dev-backend" "test"; do
-	lock=packages/"$package"/package-lock.json
-	sed -i "s#repository.engineering.redhat.com/nexus/repository/##g" "$lock"
-	git add "$lock"
+for lockDir in \
+  "./" \
+  "./packages/app" \
+  "./packages/dev" \
+  "./packages/dev-backend" \
+  "./packages/test"; do
+
+  lock="$lockDir"/package-lock.json
+  sed -i "s#repository.engineering.redhat.com/nexus/repository/##g" "$lock"
+  git add "$lock"
 done
