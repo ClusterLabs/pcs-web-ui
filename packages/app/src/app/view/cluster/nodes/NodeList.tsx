1bf23045a709a4438b63d0c22adb5abfcd3f2faa
Ivan Devat <idevat@redhat.com>
2024-04-03 14:47:22 +0200

    chore: use pf5 current node indicator

--- packages/app/src/app/view/cluster/nodes/NodeList.tsx
+++ packages/app/src/app/view/cluster/nodes/NodeList.tsx
@@ -2,13 +2,19 @@ import {DataList} from "@patternfly/react-core";
 
 import {testMarks} from "app/view/dataTest";
 import {Node} from "app/view/cluster/types";
+import {useGroupDetailViewContext} from "app/view/cluster/share";
 
 import {NodeListItem} from "./NodeListItem";
 
 const {list} = testMarks.cluster.nodes;
 export const NodeList = ({nodeList}: {nodeList: Node[]}) => {
+  const {selectedItemUrlName} = useGroupDetailViewContext();
   return (
-    <DataList {...list.mark} aria-label="Cluster nodes">
+    <DataList
+      {...list.mark}
+      aria-label="Cluster nodes"
+      selectedDataListItemId={selectedItemUrlName}
+    >
       {nodeList.map(node => (
         <NodeListItem key={node.name} node={node} />
       ))}
