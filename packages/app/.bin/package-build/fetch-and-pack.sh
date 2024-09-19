8a67e06b176e3c514994959c61fbbcec2ca83868
Ivan Devat <idevat@redhat.com>
2024-09-19 19:30:56 +0200

    feat: patch node_modules for fips

--- packages/app/.bin/package-build/fetch-and-pack.sh
+++ packages/app/.bin/package-build/fetch-and-pack.sh
@@ -10,7 +10,7 @@ fi
 
 exec="$(dirname "$0")"
 package_dir=$(realpath "$1")
-app_relative_path=$(realpath "$1")
+app_relative_path="$2"
 archive_path=$(realpath "$3")
 
 package="$package_dir"/package.json
@@ -34,10 +34,11 @@ fi
 temp_dir=$(mktemp -d)
 # We need to keep the same relative path to node_modules as from pcs-web-ui root
 # directory. So, the subdirs are created, node_modules are fetched inside the
-# subdirs and tar is done from temp dir. The result archive is placed where the
-# caller of this script specified.
+# subdirs and tar is done from the "root" temp dir. The result archive is
+# placed where the caller of this script specified.
 temp_app_dir="$temp_dir"/"$app_relative_path"
 mkdir -p "$temp_app_dir"
+node_modules="$temp_app_dir"/node_modules
 
 cp "$package_build" "$temp_app_dir"/package.json
 cp "$package_build_lock" "$temp_app_dir"/package-lock.json
@@ -45,11 +46,27 @@ if [ -f "$npmrc" ]; then
   cp "$npmrc" "$temp_app_dir"/.npmrc
 fi
 
+# Fetch node_modules.
+echo Fetching node_modules:
 npm ci --prefix="$temp_app_dir"
 
+echo \\nPatching node_modules:
+files_to_patch="\
+    babel-loader/lib/cache.js \
+  "
+substitution="s/md\(4\|5\)/sha256/g"
+for file in $files_to_patch; do
+  printf "\n[%s]:\n\n" "$file"
+  set +e
+  sed "$substitution" "$node_modules/$file" | diff "$node_modules/$file" -
+  set -e
+  sed --in-place "$substitution" "$node_modules/$file"
+done
+
+echo \\nPacking node_modules to "$archive_path":
 tar --create --xz \
   --file "$archive_path" \
   --directory "$temp_dir" \
-  "$(realpath --relative-to="$temp_dir" "$temp_app_dir"/node_modules)"
+  "$(realpath --relative-to="$temp_dir" "$node_modules")"
 
 rm -rf "$temp_dir"
