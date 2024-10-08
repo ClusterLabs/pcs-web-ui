a237c8d27bad56a7f4e564ebe05f187d631c1753
Ivan Devat <idevat@redhat.com>
2024-10-08 19:38:28 +0200

    fix: installation in `make distcheck`

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh\n\n
+#!/bin/sh
 
 destdir=$1
 pcsd_webui_dir=$2
@@ -11,12 +11,12 @@ fi
 
 if [ ! "$pcsd_webui_dir" = "" ]; then
   standalone_dir="$destdir"/"$pcsd_webui_dir"
-	mkdir -p "$standalone_dir"
-	cp -r build/for-standalone/* "$standalone_dir"
+  mkdir -p "$standalone_dir"
+  cp -r build/for-standalone/* "$standalone_dir"
 fi
 
 if [ ! "$cockpit_dir" = "" ]; then
   cockpit_ha_cluster_dir="$destdir"/"$cockpit_dir"/ha-cluster
-	mkdir -p "$cockpit_ha_cluster_dir"
-	cp -r build/for-cockpit/* "$cockpit_ha_cluster_dir"
+  mkdir -p "$cockpit_ha_cluster_dir"
+  cp -r build/for-cockpit/* "$cockpit_ha_cluster_dir"
 fi
