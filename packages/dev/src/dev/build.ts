cc20cb05affa1ede7ff9295d451b8e01bb474620
Ivan Devat <idevat@redhat.com>
2025-03-17 09:37:20 +0100

    chore: rm old assets in dev build + simplyfy code

--- packages/dev/src/dev/build.ts
+++ packages/dev/src/dev/build.ts
@@ -19,45 +19,30 @@ export const startBuild = async ({
   port: number | string;
 }) => {
   const templateDir = path.join(srcDir, structure.template.dir);
+  const staticDir = path.join(outputDir, structure.output.static);
 
   const refreshStaticFiles = async () => {
-    fs.cpSync(templateDir, outputDir, {
-      recursive: true,
-      force: true,
-    });
+    fs.cpSync(templateDir, outputDir, {recursive: true, force: true});
 
     const staticDirName = structure.output.static;
     const assetPrefix = structure.output.main;
 
-    const assetList = fs.readdirSync(
-      path.join(outputDir, structure.output.static),
-    );
+    const assetList = fs.readdirSync(staticDir);
 
     indexBuild.applyReplacements(
       path.join(outputDir, structure.template.index),
       [
         // Order is very important here! Replacements linkCSS and linkJs are
-        // oriented by <script...> placeholder and prefixing and linkJs modifies
-        // it.
-        indexBuild.linkCss({
-          assetList,
-          staticDirName,
-          assetPrefix,
-        }),
-        indexBuild.linkJs({
-          assetList,
-          staticDirName,
-          assetPrefix,
-        }),
+        // oriented by <script...> placeholder; prefix and linkJs modifies it.
+        indexBuild.linkCss({assetList, staticDirName, assetPrefix}),
+        indexBuild.linkJs({assetList, staticDirName, assetPrefix}),
         indexBuild.prefixAssetsPaths({
           pathPrefix: "/ui",
           staticDirName,
           manifest: structure.template.manifest,
           ico: structure.template.ico,
         }),
-        indexBuild.injectLiveReloadCapability({
-          port,
-        }),
+        indexBuild.injectLiveReloadCapability({port}),
       ],
     );
   };
@@ -65,34 +50,24 @@ export const startBuild = async ({
   const config = (await getConfig({
     srcDir,
     outputDir,
-    // webpack needs to know it to put the right <script> hrefs into HTML even
-    // in single-page apps that may serve index.html for nested URLs like
-    // /todos/42. We can't use a relative path in HTML because we don't want
-    // to load something like /todos/42/static/js/bundle.7289d.js. We have to
-    // know the root.
     publicPath: "./",
     nodeModules: app_node_modules,
     shouldUseSourceMap: true,
   })) as esbuild.BuildOptions;
 
-  const ctx = await esbuild.context(config);
-
-  await ctx.rebuild();
-  await refreshStaticFiles();
-
+  const ctx = await esbuild.context({
+    ...config,
+    plugins: [
+      {
+        name: "clean-and-refresh-static",
+        setup({onStart, onEnd}) {
+          onStart(() => fs.rmSync(staticDir, {recursive: true, force: true}));
+          onEnd(refreshStaticFiles);
+        },
+      },
+    ],
+  });
   await ctx.watch();
 
-  // After recompilation.
-  fs.watch(
-    path.join(outputDir, structure.output.static),
-    async (_eventType, filename) => {
-      // TODO This is "throtling" for very poor...
-      if (filename.endsWith(".js") || filename.endsWith(".css")) {
-        await refreshStaticFiles();
-      }
-    },
-  );
-
-  // After templates (e.g. packages/app/public/index.html) change.
-  fs.watch(templateDir, async () => await refreshStaticFiles());
+  fs.watch(templateDir, refreshStaticFiles);
 };
