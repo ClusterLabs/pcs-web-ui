460e8b0239fa07218b11abfd88b3afad37b6a77a
Ivan Devat <idevat@redhat.com>
2025-03-22 17:22:53 +0100

    refactor: remove unnecessary Link coplexity

--- packages/app/src/app/view/share/router/Link.tsx
+++ packages/app/src/app/view/share/router/Link.tsx
@@ -3,47 +3,24 @@ import type React from "react";
 import {useLocation} from "./Router";
 import {SimpleLink} from "./SimpleLink";
 
-export const Link = (
-  props: {
-    "data-test"?: string;
-    strong?: boolean;
-    isInline?: boolean;
-  } & (
-    | {
-        to: string;
-        children?: React.ReactNode;
-      }
-    | {
-        onClick: (
-          _navigate: ReturnType<typeof useLocation>["navigate"],
-        ) => void;
-        children: React.ReactNode;
-      }
-  ),
-) => {
+export const Link = (props: {
+  "data-test"?: string;
+  strong?: boolean;
+  isInline?: boolean;
+  to: string;
+  children?: React.ReactNode;
+}) => {
   const {navigate} = useLocation();
 
   let label = props.children;
 
-  if ("to" in props) {
-    if (!label) {
-      const parts = props.to.split("/");
-      label = parts[parts.length - 1];
-    }
-    return (
-      <SimpleLink
-        onClick={() => navigate(props.to)}
-        label={label}
-        data-test={props["data-test"]}
-        strong={props.strong}
-        isInline={props.isInline}
-      />
-    );
+  if (!label) {
+    const parts = props.to.split("/");
+    label = parts[parts.length - 1];
   }
-
   return (
     <SimpleLink
-      onClick={() => props.onClick(navigate)}
+      onClick={() => navigate(props.to)}
       label={label}
       data-test={props["data-test"]}
       strong={props.strong}
