9618e7a96a934cb5ca2d9ca75a6c35d025fb07a7
Ivan Devat <idevat@redhat.com>
2025-05-27 18:16:35 +0200

    feat: improve crm status table in resource detail

--- packages/app/src/app/view/share/CrmStatusTable.tsx
+++ packages/app/src/app/view/share/CrmStatusTable.tsx
@@ -6,6 +6,7 @@ import type {ResourceOnNodeStatus} from "app/view/cluster/types";
 
 import {StatusSign} from "./StatusSign";
 import {Table} from "./table";
+import {useWindowWidth} from "./useWindowWidth";
 
 const isRoleOk = (crmStatus: ResourceOnNodeStatus): boolean =>
   crmStatus.role === crmStatus.targetRole ||
@@ -29,57 +30,65 @@ export const CrmStatusTable = ({
     cell: (_crmStatus: ResourceOnNodeStatus) => React.ReactNode;
   };
 }) => {
+  const width = useWindowWidth();
+  // 1072 corresponds with css value 67rem. Under this threshold the group
+  // element (e.g. resource tree, node list) is hidden.
+  const tableGridClassName = width > 1072 ? "pf-m-grid-2xl" : "pf-m-grid-lg";
+
   if (crmStatusList.length === 0) {
     return <EmptyStateNoItem title={emptyMessage} canAdd={false} />;
   }
   return (
-    <Table>
-      <Thead>
+    <Table className={`pf-v5-c-table ${tableGridClassName}`}>
+      <Thead noWrap>
         <Tr>
           <Th data-label={rowObject.header}>{rowObject.header}</Th>
           <Th data-label="Active">Active</Th>
           <Th data-label="Failed">Failed</Th>
           <Th data-label="Blocked">Blocked</Th>
           <Th data-label="Managed">Managed</Th>
-          <Th data-label="Target-role">Target role</Th>
+          <Th data-label="Target-role" style={{textWrap: "wrap"}}>
+            Target role
+          </Th>
           <Th data-label="Role">Role</Th>
-          <Th data-label="Failure-ignored">Failure ignored</Th>
-          <Th data-label="Nodes-running-on">Nodes running on</Th>
+          <Th data-label="Failure-ignored" style={{textWrap: "wrap"}}>
+            Failure ignored
+          </Th>
         </Tr>
       </Thead>
       <Table.Body>
         {crmStatusList.map((crmStatus, i) => (
           <Tr key={i}>
             <th data-label={rowObject.header}>{rowObject.cell(crmStatus)}</th>
-            <Td data-label="Active">
+            <Td data-label="Active" style={{textWrap: "nowrap"}}>
               {crmStatus.active ? (
                 <StatusSign status="OK" label="Yes" showOkIco />
               ) : (
                 <StatusSign status="WARNING" label="No" />
               )}
             </Td>
-            <Td data-label="Failed">
+            <Td data-label="Failed" style={{textWrap: "nowrap"}}>
               {crmStatus.failed ? (
                 <StatusSign status="WARNING" label="Yes" />
               ) : (
                 <StatusSign status="OK" label="No" showOkIco />
               )}
             </Td>
-            <Td data-label="Blocked">
+            <Td data-label="Blocked" style={{textWrap: "nowrap"}}>
               {crmStatus.blocked ? (
                 <StatusSign status="WARNING" label="Yes" />
               ) : (
                 <StatusSign status="OK" label="No" showOkIco />
               )}
             </Td>
-            <Td data-label="Managed">
+            <Td data-label="Managed" style={{textWrap: "nowrap"}}>
               {crmStatus.managed ? (
-                <StatusSign status="OK" label="Managed" showOkIco />
+                <StatusSign status="OK" label="Yes" showOkIco />
               ) : (
-                <StatusSign status="WARNING" label="Unmanaged" />
+                <StatusSign status="WARNING" label="No" />
               )}
             </Td>
-            <Td data-label="Target-role">
+            <Td data-label="Target-role" style={{textWrap: "nowrap"}}>
               {!crmStatus.targetRole && crmStatus.targetRole}
               {crmStatus.targetRole && isTargetRoleOk(crmStatus.targetRole) && (
                 <StatusSign
@@ -93,7 +102,7 @@ export const CrmStatusTable = ({
                   <StatusSign status="WARNING" label={crmStatus.targetRole} />
                 )}
             </Td>
-            <Td data-label="Role">
+            <Td data-label="Role" style={{textWrap: "nowrap"}}>
               {isRoleOk(crmStatus) ? (
                 <StatusSign status="OK" label={crmStatus.role} showOkIco />
               ) : (
@@ -101,9 +110,8 @@ export const CrmStatusTable = ({
               )}
             </Td>
             <Td data-label="Failure-ignored">
-              {crmStatus.failureIgnored ? "yes" : "no"}
+              {crmStatus.failureIgnored ? "Yes" : "No"}
             </Td>
-            <Td data-label="Nodes-running-on">{crmStatus.nodesRunningOn}</Td>
           </Tr>
         ))}
       </Table.Body>
---
+++ packages/app/src/app/view/share/useWindowWidth.ts
@@ -0,0 +1,16 @@
+import {useEffect, useState} from "react";
+
+export const useWindowWidth = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return width;
+};
