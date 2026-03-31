cad66bdb4733dfeed7809900a818d3c6bf717174
Ivan Devat <idevat@redhat.com>
2026-03-31 16:59:41 +0200

    build: fix npm registry url

--- .bin/pre-commit/normalize-npm-registry.sh
+++ .bin/pre-commit/normalize-npm-registry.sh
@@ -7,8 +7,13 @@
 for npm_dir in $NPM_DIRS; do
   lock_file=$npm_dir/package-lock.json
 
+  # Strip trailing slash from detected registry so that the replacement
+  # does not eat the path separator in resolved URLs
+  # (e.g. "https://registry.npmjs.org/" -> "https://registry.npmjs.org").
+  current_registry=$(npm config get registry --prefix "$npm_dir" | sed 's#/$##')
+
   sed -i \
-    "s#$(npm config get registry --prefix "$npm_dir")#$STD_NPM_REGISTRY#g" \
+    "s#$current_registry#$STD_NPM_REGISTRY#g" \
     "$lock_file"
   git add "$lock_file"
 done
