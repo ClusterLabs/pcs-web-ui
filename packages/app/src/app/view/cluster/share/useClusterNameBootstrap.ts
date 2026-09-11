b1aa7b7fc6d425753c41b53eb7ebee92883ae531
Ivan Devat <idevat@redhat.com>
2026-09-11 16:29:51 +0200

    fix: propagate unauthorized status loading to store

--- packages/app/src/app/view/cluster/share/useClusterNameBootstrap.ts
+++ packages/app/src/app/view/cluster/share/useClusterNameBootstrap.ts
@@ -1,16 +1,21 @@
 import React from "react";
 
 import {type api, clusterStatus} from "app/backend";
+import {useDispatch} from "app/view/share";
 
 type BootstrapState =
   | {status: "loading"}
   | {
       status: "error";
-      result: Exclude<api.ResultOf<typeof clusterStatus>, {type: "OK"}>;
+      result: Exclude<
+        api.ResultOf<typeof clusterStatus>,
+        {type: "OK"} | {type: "UNAUTHORIZED"}
+      >;
     }
   | {status: "ok"; clusterName: string};
 
 export const useClusterNameBootstrap = (): BootstrapState => {
+  const dispatch = useDispatch();
   const [state, setState] = React.useState<BootstrapState>({
     status: "loading",
   });
@@ -30,6 +35,8 @@ export const useClusterNameBootstrap = (): BootstrapState => {
           status: "ok",
           clusterName: result.payload.cluster_name,
         });
+      } else if (result.type === "UNAUTHORIZED") {
+        dispatch({type: "AUTH.REQUIRED"});
       } else {
         setState({
           status: "error",
@@ -43,7 +50,7 @@ export const useClusterNameBootstrap = (): BootstrapState => {
     return () => {
       cancelled = true;
     };
-  }, []);
+  }, [dispatch]);
 
   return state;
 };
